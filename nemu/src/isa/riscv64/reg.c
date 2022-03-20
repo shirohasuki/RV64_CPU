44c0d35f6418f33cc408e417c9144150a9eb0a66
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 12:31:03 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:31:03 up 1 day, 14:45,  1 user,  load average: 0.44, 0.37, 0.36

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -9,8 +9,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  //enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
-  printf("EAX:0x%lx\n",gpr(0));
+  enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
+  for (int i = 0; i < 8;i++) {
+    printf("%s:0x%lx\n", regs[i], gpr(i));
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {

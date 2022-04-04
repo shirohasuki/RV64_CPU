058da12e174a85d1f070e229f9388ae4a090a63b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 15:58:42 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:58:42 up 22:46,  1 user,  load average: 0.50, 0.92, 0.79

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -11,7 +11,7 @@ typedef struct watchpoint {
 } WP;
 
 static WP wp_pool[NR_WP] = {}; //线程池
-static WP *head = NULL, *free_ = NULL;
+static WP *head = NULL, *free_ = NULL; //链表（头节点）
 //head:激活的监视点的链表的头节点;free:空闲的监视点的链表的头节点
 
 void init_wp_pool() {
@@ -37,8 +37,13 @@ WP *new_wp() {
     return tmp;
 } // free -> head
 
-
-void free_wp(WP *wp) {
+void free_wp(WP *wp) { //传入节点
     if (!wp) { return ;}
-    
-}
+    WP *tmp = head;
+    if (tmp == wp) { head->next = NULL;}//判断首个节点为
+    while (tmp->next != wp) { tmp = tmp->next;}//tmp到达删除处
+    tmp->next = tmp->next->next; //剔除wp
+
+    wp->next = free_; 
+    free_ = wp;// wp->next一定指向free头，之前操作只是作用于head链表内部逻辑 
+} //将head链表中指定的wp拿出插入free_

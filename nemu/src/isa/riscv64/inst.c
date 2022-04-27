c08b286ea58a8aa8b5f8204cf446ef9e32951f93
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-27 19:44:30 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:44:30 up  8:08,  1 user,  load average: 0.40, 0.49, 0.40

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -34,9 +34,9 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     destR(rd);
     switch (type) {
         case TYPE_R: src1R(rs1);     src2R(rs2);    break; //add
-        case TYPE_I:   src2I(immI(i));    src1R(rs1);   break;
+        case TYPE_I: src1R(rs1);     src2I(immI(i));    break;
         case TYPE_S: destI(immS(i)); src1R(rs1);    src2R(rs2);     break;
-        case TYPE_B: src1R(rs1);     src2R(rs2);    src1I(immB(i));   break; // add
+        case TYPE_B: destI(immB(i)); src1R(rs1);    src2R(rs2);    src1I(immB(i));   break; // add
         case TYPE_U: src1I(immU(i));    break;
         case TYPE_J: src1I(immJ(i));    break; // add
         

440f12f982d566260071a6d6301a7fdd44765d4d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-30 08:17:45 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:17:45 up  8:21,  1 user,  load average: 0.41, 0.42, 0.42

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -75,7 +75,7 @@ static int decode_exec(Decode *s) {
 
     INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if (src1 == src2) s->pc += immB(INSTPAT_INST(s)));
     //beq 是相等条件分支，rs1 和 rs2 的值相等时，把 pc 的值设置成当前值+偏移值；
-    INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, if (src1 != src2) s->snpc += immB(INSTPAT_INST(s)));
+    INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, if (src1 != src2) s->snpc += immB(INSTPAT_INST(s))); // 不知道为什么snpc就过了
     //bne 是不等条件分支，rs1 和 rs2 的值不等时，把 pc 的值设置成当前值+偏移值；
     //blt 是小于条件分支，rs1 小于 rs2 的值时，把 pc 的值设置成当前值+偏移值；
     //bge是大于等于条件分支，rs1 大于等于 rs2 的值时，把 pc 的值设置成当前值+偏移值；
@@ -87,7 +87,9 @@ static int decode_exec(Decode *s) {
     // jalr:跳转并链接，把 pc 设置成 rs1+偏移值，然后将 pc+4 写入 rd 中
     
     INSTPAT("??????? ????? ????? 000 ????? 01110 11", addw   , I, R(dest) = src1 + src2); //结果截断为32位是什么？
-    //addw:把寄存器 x[rs2]加到寄存器 x[rs1]上, 将结果截断为 32 位, 把符号位扩展的结果写入 x[rd]
+    // addw:把寄存器 x[rs2]加到寄存器 x[rs1]上, 将结果截断为 32 位, 把符号位扩展的结果写入 x[rd]
+    INSTPAT("??????? ????? ????? 000 ????? 00110 11", addiw   ,I, R(dest) = src1 + immI(INSTPAT_INST(s))); //结果截断为32位是什么？
+    // addiw:把 立即数 加到寄存器 x[rs1]上, 将结果截断为 32 位, 把符号位扩展的结果写入 x[rd]
 
     //lb 是字节加载，读取一个字节写入 rd 中
     //lh 是半字加载，读取两个字节写入 rd 中

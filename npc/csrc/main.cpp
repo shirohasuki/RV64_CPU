1a81cf04628676f0e60286d2ea23e8c758f3189e
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 04:00:35 +0800

    >  sim RTL ysyx_22040058 王晖 Linux shiroha 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:00:35 up  4:45,  1 user,  load average: 0.70, 0.68, 0.57

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -71,7 +71,7 @@ void sim_exit() {
 
 int main() {
     sim_init();
-    int i = 10;
+    int i = 20000;
     while (i--) {
         top->rst = 1;   top->clk = 0;   step_and_dump_wave();
                         top->clk = 1;   step_and_dump_wave();

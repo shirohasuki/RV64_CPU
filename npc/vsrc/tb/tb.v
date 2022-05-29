0e767581ddb52ffd9a91af268ee45034f56f6de9
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 03:27:42 +0800

    >  sim RTL ysyx_22040058 王晖 Linux shiroha 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:27:42 up  4:12,  1 user,  load average: 0.28, 0.31, 0.44

--- npc/vsrc/tb/tb.v
+++ npc/vsrc/tb/tb.v
@@ -9,9 +9,9 @@ module tb(
     input rst 
 );
 
-    wire x3 = tb.soc_inst.riscv_inst.regs_inst.regs[3];
-	wire x26 = tb.soc_inst.riscv_inst.regs_inst.regs[26];
-	wire x27 = tb.soc_inst.riscv_inst.regs_inst.regs[27];
+    wire x3 = tb.soc_inst.riscv_inst.regs_inst.regs[3];     // x3:表示在第几个测试
+	wire x26 = tb.soc_inst.riscv_inst.regs_inst.regs[26];   // x26:表示测试结束
+	wire x27 = tb.soc_inst.riscv_inst.regs_inst.regs[27];   // x27:表示是否成功
 	
 	//rom 初始值
 	initial begin
@@ -19,18 +19,20 @@ module tb(
 	end
 
 	initial begin
-		//wait(x26 == 32'b1);
+		while (x26 == 32'b1) begin
+            if(x27 == 32'b1) begin
+                $display("########  pass  !!!#########");
+            end
+            else begin
+                $display("########  fail  !!!#########");
+                $display("fail testnum = %2d", x3);
+                for (integer r = 0;r < 32; r = r + 1)begin
+                    $display("x%2d register value is %d",r,tb.soc_inst.riscv_inst.regs_inst.regs[r]);	
+                end	
+            end
+        end
 
-		if(x27 == 32'b1) begin
-			$display("########  pass  !!!#########");
-		end
-		else begin
-			$display("########  fail  !!!#########");
-			$display("fail testnum = %2d", x3);
-			for (integer r = 0;r < 32; r = r + 1)begin
-				$display("x%2d register value is %d",r,tb.soc_inst.riscv_inst.regs_inst.regs[r]);	
-			end	
-		end
+		
 	end
 
     initial begin

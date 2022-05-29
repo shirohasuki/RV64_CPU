e06d533184ae7aacc9eb3feefec50b41d0624489
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-29 17:15:34 +0800

    >  sim RTL ysyx_22040058 王晖 Linux shiroha 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:15:34 up  2:43,  1 user,  load average: 0.70, 0.85, 0.81

--- npc/build/obj_dir/Vtb___024root__DepSet_hfe20aad3__0__Slow.cpp
+++ npc/build/obj_dir/Vtb___024root__DepSet_hfe20aad3__0__Slow.cpp
@@ -12,17 +12,18 @@ VL_ATTR_COLD void Vtb___024root___initial__TOP__0(Vtb___024root* vlSelf) {
     Vtb__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
     VL_DEBUG_IF(VL_DBG_MSGF("+    Vtb___024root___initial__TOP__0\n"); );
     // Init
-    VlWide<6>/*191:0*/ __Vtemp_hf32e9773__0;
+    VlWide<7>/*223:0*/ __Vtemp_h6d578ef8__0;
     // Body
     vlSymsp->_vm_contextp__->dumpfile(std::string("wave.vcd"));
     vlSymsp->_traceDumpOpen();
-    __Vtemp_hf32e9773__0[0U] = 0x2e747874U;
-    __Vtemp_hf32e9773__0[1U] = 0x5f414444U;
-    __Vtemp_hf32e9773__0[2U] = 0x64617461U;
-    __Vtemp_hf32e9773__0[3U] = 0x6e73745fU;
-    __Vtemp_hf32e9773__0[4U] = 0x74622f69U;
-    __Vtemp_hf32e9773__0[5U] = 0x2e2fU;
-    VL_READMEM_N(false, 32, 4096, 0, VL_CVT_PACK_STR_NW(6, __Vtemp_hf32e9773__0)
+    __Vtemp_h6d578ef8__0[0U] = 0x2e747874U;
+    __Vtemp_h6d578ef8__0[1U] = 0x5f414444U;
+    __Vtemp_h6d578ef8__0[2U] = 0x64617461U;
+    __Vtemp_h6d578ef8__0[3U] = 0x6e73745fU;
+    __Vtemp_h6d578ef8__0[4U] = 0x74622f69U;
+    __Vtemp_h6d578ef8__0[5U] = 0x7372632fU;
+    __Vtemp_h6d578ef8__0[6U] = 0x2e2f76U;
+    VL_READMEM_N(false, 32, 4096, 0, VL_CVT_PACK_STR_NW(7, __Vtemp_h6d578ef8__0)
                  ,  &(vlSelf->tb__DOT__soc_inst__DOT__rom_inst__DOT__rom_mem)
                  , 0, ~0ULL);
 }

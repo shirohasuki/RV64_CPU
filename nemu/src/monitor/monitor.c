d30d5f83fb360ccefde6caf8f82a458044cea128
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 21:28:07 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:07 up 10 days,  9:58,  1 user,  load average: 0.58, 0.64, 0.78

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -72,7 +72,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'e': elf_file = "/home/shiroha/Code/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/build/add-riscv64-nemu.elf"; break;		// ELF read
+      case 'e': printf("hello");elf_file = "/home/shiroha/Code/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/build/add-riscv64-nemu.elf"; break;		// ELF read
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);

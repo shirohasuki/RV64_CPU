80a750900f30030a722ec9956f692be3f376c33f
22040058 Wang Hui <3202778076@qq.com>
2022-08-17 19:54:13 +0800

    finish klib

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,13 +5,71 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+
+char* num2str(char *str, ll num, ll base) {
+    char tmp[32];
+    if (num < 0) {
+        *str++ = '-';
+        num = -num;
+    }
+    int len = 0;
+    if (num == 0) tmp[len++] = 0;
+    else while (num) {
+        tmp[len++] = num % base;
+        num = num / base;
+    }
+    while (len-- > 0) {
+        if (tmp[len] < 10) *str++ = tmp[len] + '0';
+        else *str++ = tmp[len] - 10 + 'A';
+    }
+    return str;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+    int n;
+    char buf[8192];
+    va_list args;
+    va_start(args, fmt);
+    n = vsprintf(buf, fmt, args);
+    va_end(args);
+    putstr(buf); //  basic output function
+    return n;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    char *s;
+    char *str;
+    for (str = out; *fmt; fmt++) {
+        if (*fmt != '%') {
+            *str++ = *fmt;
+            continue;
+        }
+        fmt++;
+    switch (*fmt) {
+        case 'c':
+            *str++ = va_arg(ap, int);
+            break;
+        case 'd':
+            str = num2str(str, va_arg(ap, ll), 10);
+            break;
+        case 'p':
+            str = num2str(str, va_arg(ap, ll), 16);
+            break;
+        case 's':
+            s = va_arg(ap, char *);
+            while (*s) *str++ = *s++;
+            break;
+        default:
+            if (*fmt != '%') *str++ = '%';
+            if (*fmt) *str++ = *fmt;
+            else fmt--;
+            break;
+        }
+    }
+    *str = '\0';
+    return str - out;
 }
+
 
 int sprintf(char *out, const char *fmt, ...) {
   panic("Not implemented");
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,99 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+// 指针大小为size_t,八字节
+
 size_t strlen(const char *s) {
-  panic("Not implemented");
+    size_t len;
+    while (s[len] != '\0') {
+        len++;
+    }
+    return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+    for (size_t i = 0; src[i] != '\0'; i++) {
+        dst[i] = src[i];
+    }
+    ++dst = '\0';
+    return dst; 
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dst[i] = src[i];
+    }
+    for (; i < n; ++i) {
+		dst[i] = '\0';
+	} // 之后全部填充'\0'
+    return dst; 
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
-}
+	size_t dst_len = strlen(dst), i;
+	for (i = 0; src[i] != '\0'; i++) {
+		dst[dst_len + i] = src[i];
+	}
+	dst[dst_len + i] = '\0';
+	return dst;
+} //append
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  	size_t i = 0;
+	for (; s1[i] != '\0' && s2[i] != '\0'; ++i) {
+		if (s1[i] != s2[i]) return s1[i] - s2[i];
+	}
+	return s1[i] - s2[i];
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
-}
+  	size_t i = 0;
+	for (; s1[i] != '\0' && s2[i] != '\0' && i < n; ++i) {
+		if (s1[i] != s2[i]) return s1[i] - s2[i];
+	}
+	if (i == n) return 0;
+	return s1[i] - s2[i];  
+} // 只比较前n位
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+    char *prt = s;
+    while (n--) {
+        *prt++ = c; 
+    }
+    return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+    char *ptrd = dst;
+	const char *ptrs = src;
+	if (dst < src) {
+		while (n--) {
+			*ptrd++ = *ptrs++;
+		}
+	} else {
+		while (n--) {
+			*(ptrd + n) = *(ptrs + n);
+		}
+	}
+	return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  	char *ptrd = out;
+	const char *ptrs = in;
+	while (n--) {
+		*ptrd++ = *ptrs++;
+	}
+	return out;  
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+    const unsigned char *ptr1 = s1, *ptr2 = s2;  
+    while (n--) {
+        if (*ptr1 != *ptr2) return *ptr1 - *ptr2;
+    }
+    return *ptr1 - *ptr2;
 }
 
 #endif

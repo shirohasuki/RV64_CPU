2cd90da244cf7325f9417af607680edad06032f9
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 20:49:54 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:49:54 up 21:10,  1 user,  load average: 1.46, 2.28, 1.99

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -5,15 +5,17 @@
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   assert(0);
-}
+}// 在DUT host memory的`buf`和REF guest memory的`dest`之间拷贝`n`字节,
+// `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
 
 void difftest_regcpy(void *dut, bool direction) {
   assert(0);
-}
+}// `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
+// `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
 
 void difftest_exec(uint64_t n) {
   assert(0);
-}
+}// 让REF执行`n`条指令
 
 void difftest_raise_intr(word_t NO) {
   assert(0);
@@ -22,4 +24,4 @@ void difftest_raise_intr(word_t NO) {
 void difftest_init(int port) {
   /* Perform ISA dependent initialization. */
   init_isa();
-}
+}// 初始化REF的DiffTest功能

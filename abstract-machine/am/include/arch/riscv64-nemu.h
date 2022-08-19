ceec1d472ff31cad8d41503f13decba64b3afbf4
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 23:07:56 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:07:56 up 23:28,  1 user,  load average: 0.46, 0.56, 0.72

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -3,13 +3,13 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t gpr[32], mcause, mstatus, mepc;
+  uintptr_t mepc, mcause, gpr[32], mstatus;
   void *pdir;
-}; // PA2.2 difftest
+}; // PA2.2 difftest finished
 
 #define GPR1 gpr[17] // a7
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR2 gpr[10] // a0
+#define GPR3 gpr[11] // a1
+#define GPR4 gpr[12] // a2
+#define GPRx gpr[10] // a0
 #endif
--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -4,9 +4,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-    cpu.csr[mepc] = epc;
-    cpu.csr[mcause] = NO;
-    return cpu.csr[mtvec];
+    // cpu.csr[mepc] = epc;
+    // cpu.csr[mcause] = NO;
+    // return cpu.csr[mtvec];
+    return 0;
 }
 
 word_t isa_query_intr() {

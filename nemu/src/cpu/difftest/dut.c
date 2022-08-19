7716f6e12a23aa712cc46775a0fa0f6ab438be03
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-20 00:36:40 +0800

    >  compile NEMU ysyx_22040058 王晖 Linux shiroha 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:36:40 up 1 day, 56 min,  1 user,  load average: 1.18, 1.02, 0.66

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -68,7 +68,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
   Log("Differential testing: %s", ASNI_FMT("ON", ASNI_FG_GREEN));
   Log("The result of every instruction will be compared with %s. "
-      "This will help you a lot for debugging, but also significantly reduce the performance. "
+      //"This will help you a lot for debugging, but also significantly reduce the performance. "
       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
 
   ref_difftest_init(port);

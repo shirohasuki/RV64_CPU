0001d5cb0bdbee95d0ad77a877e6812e55cab776
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-24 21:59:35 +0800

    > compile NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:59:35 up 11:05,  1 user,  load average: 0.98, 0.56, 0.30

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -7,7 +7,14 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
     // cpu.csr[mepc] = epc;
     // cpu.csr[mcause] = NO;
     // return cpu.csr[mtvec];
-    return 0;
+    // return 0;
+  cpu.csr[mepc] = epc;
+  cpu.csr[mcause] = NO;
+#ifdef CONFIG_ETRACE
+  printf("[ETrace] Exception: mtvec = 0x%016lx, mepc = 0x%016lx, mstatus = 0x%016lx, mcause = 0x%016lx\n",
+    cpu.csr[mtvec], cpu.csr[mepc], cpu.csr[mstatus], cpu.csr[mcause]);
+#endif
+  return cpu.csr[mtvec];
 }
 
 word_t isa_query_intr() {

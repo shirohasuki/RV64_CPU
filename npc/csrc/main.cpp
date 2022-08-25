eca0aecdfdeb0e5a53934f4c91ffe0b032460383
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-25 20:08:43 +0800

    > sim RTL ysyx_22040058 王晖 Linux shiroha 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:08:43 up  1:40,  1 user,  load average: 0.41, 0.70, 0.80

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,48 +1,5 @@
-/*
-#include "verilated.h"
-#include "verilated_vcd_c.h"
-#include "Vriscv.h"
-
-VerilatedContext* contextp = NULL;
-VerilatedVcdC* tfp = NULL;
-
-static Vriscv* top;
-
-void step_and_dump_wave() {
-    top->eval();
-    contextp->timeInc(1);
-    tfp->dump(contextp->time());
-}
-void sim_init() {
-    contextp = new VerilatedContext;
-    tfp = new VerilatedVcdC;
-    top = new Vriscv;
-    contextp->traceEverOn(true);
-    top->trace(tfp, 0);
-    tfp->open("dump.vcd");
-}
-
-void sim_exit() {
-    step_and_dump_wave();
-    tfp->close();
-}
-
-int main() {
-    sim_init();
-    top->inst_i = 00000010011000000000110110010011; top->rst =1; top->clk = 0; step_and_dump_wave();
-                                                    top->rst =1; top->clk = 1; step_and_dump_wave();
-    top->inst_i = 00000111100000000000111000010011; top->rst =1; top->clk = 0; step_and_dump_wave();
-                                                    top->rst =1; top->clk = 1; step_and_dump_wave();
-    top->inst_i = 00000001110011011000111010110011; top->rst =1; top->clk = 0; step_and_dump_wave();
-                                                    top->rst =1; top->clk = 1; step_and_dump_wave();
-
-    sim_exit();
-} 
-*/
-
-
-#include "verilated.h"
-#include "verilated_vcd_c.h"
+#include <verilated.h>
+#include <verilated_vcd_c.h>
 #include "Vtb.h"
 
 VerilatedContext* contextp = NULL;

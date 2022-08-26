1bef7397d1d184a169bc2f40361dcb5bff9053c6
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-26 22:47:57 +0800

    > compile NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:47:57 up 15:06,  1 user,  load average: 1.34, 1.61, 1.58

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -34,25 +34,25 @@ static char *img_file = NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
-  if (img_file == NULL) {
-    Log("No image is given. Use the default build-in image.");
-    return 4096; // built-in image size
-  }
+    if (img_file == NULL) {
+        Log("No image is given. Use the default build-in image.");
+        return 4096; // built-in image size
+    }
 
-  FILE *fp = fopen(img_file, "rb");
-  Assert(fp, "Can not open '%s'", img_file);
+    FILE *fp = fopen(img_file, "rb");
+    Assert(fp, "Can not open '%s'", img_file);
 
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
 
-  Log("The image is %s, size = %ld", img_file, size);
+    Log("The image is %s, size = %ld", img_file, size);
 
-  fseek(fp, 0, SEEK_SET);
-  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
-  assert(ret == 1);
+    fseek(fp, 0, SEEK_SET);
+    int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+    assert(ret == 1);
 
-  fclose(fp);
-  return size;
+    fclose(fp);
+    return size;
 }
 
 static int parse_args(int argc, char *argv[]) {

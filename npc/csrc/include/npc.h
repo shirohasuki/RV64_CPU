6ef7df1f80611b7094cb27f97462117bd31c9ca2
22040058 Wang Hui <3202778076@qq.com>
2022-09-14 20:16:24 +0800

    finish itrace

--- npc/csrc/include/npc.h
+++ npc/csrc/include/npc.h
@@ -10,14 +10,14 @@
 #include <assert.h> 
 #include <stdlib.h> 
 
-// #define CONFIG_DIFFTEST 1
-// #define CONFIG_VCD 1
-#define CONFIG_ITRACE 1
-// #define CONFIG_MTRACE 1
-// #define CONFIG_FTRACE 1
 
+#define CONFIG_NPC_ITRACE 1
+// #define CONFIG_NPC_DIFFTEST 1
 
 typedef long long ll;
+
+// ================ CPU ===================
+static int status = 0;
 
 // =============== Memory ===============
 #define MEM_BASE 0x80000000
@@ -28,4 +28,17 @@ static uint8_t mem[MEM_SIZE];
 uint8_t* cpu2mem(ll addr);
 
 long load_image(char const *img_file); 
+// ============== Reg ===================
+extern uint64_t *cpu_gpr;
+// static uint64_t cpu_pc = 80000000;
+extern uint64_t cpu_pc;
 
+// ============= ITRACE ================
+void init_disasm(const char *triple);
+void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+// void disassemble(char *str, uint64_t pc, uint8_t *code, int nbyte);
+void itrace_record(uint64_t pc);
+void itrace_output();
+void dump_gpr(); // 打印寄存器
+
+// ============= Format ================

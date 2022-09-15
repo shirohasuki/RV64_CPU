4eb585ab2602ab7026ecc99371f00f4cbdf1dff0
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-15 20:32:04 +0800

    > compile NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:32:04 up  1:19,  1 user,  load average: 0.40, 0.51, 0.54

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,7 +51,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-    printf("%x\n", addr);
+    // printf("%x\n", addr);
     if (likely(in_pmem(addr))) return pmem_read(addr, len);
     IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
     out_of_bound(addr);
--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -2,13 +2,14 @@
 #include <memory/paddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+    printf("1.[vaddr.c]%lx\n", addr);
+    return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+    return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  paddr_write(addr, len, data);
+    paddr_write(addr, len, data);
 }

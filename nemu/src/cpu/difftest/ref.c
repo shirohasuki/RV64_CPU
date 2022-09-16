2f20f02e7ff7826a287b18bbd0fd7653e7cca418
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-16 08:07:06 +0800

    > compile NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:07:06 up 12:54,  1 user,  load average: 0.60, 0.63, 0.63

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -12,27 +12,38 @@ void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
 }// 在DUT host memory的`buf`和REF guest memory的`dest`之间拷贝`n`字节,
 // `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
 
+// void difftest_regcpy(void *dut, bool direction) {
+//     if (direction == DIFFTEST_TO_REF) {
+//         cpu.pc = ((uint64_t*)dut)[32];
+//         printf("-1.ref: pc = %lx\n", cpu.pc);
+//         for (int i = 0; i < 32; i++) {
+//             cpu.gpr[i] = ((uint64_t*)dut)[i];
+//         }
+//         for (int i = 0; i < 4; i++) {
+//             cpu.csr[i] = ((uint64_t*)dut)[i];
+//         }
+//     } // `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`; 
+//     else {
+//         ((uint64_t*)dut)[32] = cpu.pc;
+//         for (int i = 0; i < 32; i++) {
+//             ((uint64_t*)dut)[i] = cpu.gpr[i];
+//         }
+//         for (int i = 0; i < 4; i++) {
+//             // printf("cpu.csr: %ld ", cpu.csr[i]);
+//             ((uint64_t*)dut)[i] = cpu.csr[i];
+//         }
+//     } // `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
+// }
+
 void difftest_regcpy(void *dut, bool direction) {
-    if (direction == DIFFTEST_TO_REF) {
-        cpu.pc = ((uint64_t*)dut)[32];
-        printf("-1.ref:pc = %lx\n", cpu.pc);
-        for (int i = 0; i < 32; i++) {
-            cpu.gpr[i] = ((uint64_t*)dut)[i];
-        }
-        for (int i = 0; i < 4; i++) {
-            cpu.csr[i] = ((uint64_t*)dut)[i];
-        }
-    } // `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`; 
+    if (direction == DIFFTEST_TO_DUT) {
+        for (int i = 0; i < 32; i++)
+            ((CPU_state *)dut)->gpr[i] = cpu.gpr[i];
+        ((CPU_state *)dut)->pc = cpu.pc;
+    }
     else {
-        ((uint64_t*)dut)[32] = cpu.pc;
-        for (int i = 0; i < 32; i++) {
-            ((uint64_t*)dut)[i] = cpu.gpr[i];
-        }
-        for (int i = 0; i < 4; i++) {
-            // printf("cpu.csr: %ld ", cpu.csr[i]);
-            ((uint64_t*)dut)[i] = cpu.csr[i];
-        }
-    } // `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
+        cpu = *(CPU_state *)dut;
+    }
 }
 
 

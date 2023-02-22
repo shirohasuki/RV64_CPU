24c3c341869198ca520a5ec72a8c3a087c6aa273
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 00:25:09 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:25:09 up 1 day,  3:14,  1 user,  load average: 0.69, 0.54, 0.54

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
     printf("event=%d, EVENT_SYSCALL=%d\n", e.event, EVENT_SYSCALL);
     switch (e.event) {
-       case EVENT_SYSCALL: do_syscall(c); c->mepc = c->mepc+4; break;
+       case EVENT_SYSCALL: printf("here");do_syscall(c); c->mepc = c->mepc+4; break;
        case EVENT_YIELD: printf("irq event yield.\n");  c->mepc = c->mepc+4; break;
        case EVENT_ERROR: panic("irq event error.\n"); break;
        default: panic("Unhandled event ID = %d", e.event); break;

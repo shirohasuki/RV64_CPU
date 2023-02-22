66540ff5f3f8129763ee142f42f21cdcf7d776d4
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 00:18:58 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:18:58 up 1 day,  3:08,  1 user,  load average: 0.76, 0.64, 0.58

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,13 +8,13 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case -1: ev.event = EVENT_YIELD; break;   // yield
-      case  0: ev.event = EVENT_SYSCALL; break; // exit
-      case  1: ev.event = EVENT_SYSCALL; break; // yield
-      case  4: ev.event = EVENT_SYSCALL; break; // write
-      default: ev.event = EVENT_ERROR; break;
+        case -1: ev.event = EVENT_YIELD; break;   // yield
+        case  0: ev.event = EVENT_SYSCALL; break; // exit
+        case  1: ev.event = EVENT_SYSCALL; break; // yield
+        case  4: ev.event = EVENT_SYSCALL; break; // write
+        default: ev.event = EVENT_ERROR; break;
     }
-    // printf("%d\n", c->mcause);
+    printf("%d\n", c->mcause);
     c = user_handler(ev, c);
     assert(c != NULL);
   }

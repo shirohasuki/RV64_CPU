b7d8326eb2ef4903d8ac0f5ba30ff5438f233a40
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 01:37:36 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:37:36 up 2 days,  4:26,  1 user,  load average: 0.64, 0.53, 0.63

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,10 +2,10 @@
 #include "NDL.h"
 
 int main() {
-  int i=0;
-  while(NDL_GetTicks()/500){
-    printf("%d time has gone 0.5s\n",i);
-    i++;
-  }
-  return 0;
+    int i=0;
+    while(NDL_GetTicks()/500){
+        printf("%d time has gone 0.5s\n",i);
+        i++;
+    }
+    return 0;
 }

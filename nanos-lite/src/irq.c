bad061e995bc44f39fe5874af6a3e383c25a5137
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 01:18:33 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:18:33 up 2 days,  4:07,  1 user,  load average: 1.17, 1.04, 0.75

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
-    // printf("Event=%d\n", e.event);
+    printf("Event=%d\n", e.event);
     switch (e.event) {
        case EVENT_SYSCALL: do_syscall(c);break;// c->mepc = c->mepc+4; break;
        case EVENT_YIELD: printf("irq event yield.\n"); break; //c->mepc = c->mepc+4; break;

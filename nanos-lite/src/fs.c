374bdc339ce9d01f9c5acabc2feb37323944b4c4
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 21:07:50 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:07:50 up 1 day, 23:57,  1 user,  load average: 0.65, 0.74, 0.61

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,12 @@
 #include <fs.h>
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t serial_write(const void *buf, size_t offset, size_t len);
+size_t events_read(void *buf, size_t offset, size_t len);
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
+
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -9,7 +16,7 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
-} Finfo;
+} Finfo; // 文件记录表
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
@@ -28,9 +35,87 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, invalid_read, fb_write},
 #include "files.h"
 };
 
+
 void init_fs() {
-  // TODO: initialize the size of /dev/fb
+  // initialize the size of /dev/fb
+  AM_GPU_CONFIG_T dispinfo = io_read(AM_GPU_CONFIG);
+  
+  // method 1: only write w for one time, and use loop to finish all, slow but support native.
+  // file_table[FD_FB].size = dispinfo.width * dispinfo.height * 4;  // 4 for 32bit!
+
+  // method 2: use high 32bit to store w, low 32bit to store h. fast but not support native!
+  file_table[FD_FB].size = dispinfo.width * dispinfo.height;  // 4 for 32bit!
+}
+
+char* getFinfoName(int i){
+  return file_table[i].name;
+}
+
+#define TABLE_LEN (int)(sizeof(file_table) / sizeof(Finfo))
+
+size_t open_offset = 0;
+
+int fs_open(const char *pathname, int flags, int mode){
+  for(int i=0; i<TABLE_LEN; i++) {
+    if(strcmp(file_table[i].name,pathname)==0){
+      open_offset = 0;
+      return i;
+    }
+  }
+  return -1;
+}
+
+size_t fs_read(int fd, void *buf, size_t len){
+  if(file_table[fd].read != NULL){
+    return (file_table[fd].read)(buf,0,len);
+  }
+  else{
+    size_t f_size = file_table[fd].size;
+    if(open_offset >= f_size){
+      return -1;
+    }
+    if(open_offset + len > f_size){
+      len = f_size - open_offset;
+    }
+    ramdisk_read(buf, file_table[fd].disk_offset + open_offset, len);
+    open_offset = open_offset + len;
+    return len;
+  }
+}
+
+size_t fs_write(int fd, const void *buf, size_t len){
+  if(file_table[fd].write != NULL){
+    return (file_table[fd].write)(buf,open_offset,len);
+  }
+  else{
+    size_t f_size = file_table[fd].size;
+    if(open_offset >= f_size){
+      return -1;
+    }
+    if(open_offset + len > f_size){
+      len = f_size - open_offset;
+    }
+    ramdisk_write(buf, file_table[fd].disk_offset + open_offset, len);
+    open_offset = open_offset + len;
+    return len;
+  }
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence){
+    size_t f_size = file_table[fd].size;
+    switch (whence) {
+        case SEEK_SET: if(offset <= f_size) {open_offset = offset; return open_offset;} else{ return -1;} break;
+        case SEEK_CUR: if(open_offset + offset <= f_size) {open_offset = open_offset + offset; return open_offset;} else{ return -1;} break; 
+        case SEEK_END: if((signed)offset <= 0)  { open_offset = f_size + offset; return open_offset;} else { return -1; } break;
+        default: panic("check your input whence for lseek"); break;
+    }
+}
+
+int fs_close(int fd){
+  open_offset = 0;
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,8 @@
 #include <proc.h>
 #include <elf.h>
+#include <stdio.h>
+#include "fs.h"
+
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -27,30 +30,63 @@ size_t get_ramdisk_size();
 
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-    // ELF Header
-    Elf_Ehdr *elf = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
-    ramdisk_read(elf, 0, sizeof(Elf_Ehdr));
-    // Check ELF magic number
-    Log("elf->e_ident = 0x%p", *(uint32_t *)elf->e_ident);
-    assert(*(uint32_t *)elf->e_ident == 0x464C457F);
-    // Check ELF machine
-    Log("elf->e_machine = 0x%p", elf->e_machine);
-    assert(EXPECT_TYPE == elf->e_machine);
-    // Program Header
-    Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf->e_phnum);
-    ramdisk_read(phdr, elf->e_phoff, sizeof(Elf_Phdr) * elf->e_phnum);
-    // Analysis of program header table
-    for (int i = 0; i < elf->e_phnum; ++i) {
-        if (phdr[i].p_type != PT_LOAD) continue;
-        ramdisk_read((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
-        memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
-    }
-    return elf->e_entry;
+    // ramdisk version
+    // // ELF Header
+    // Elf_Ehdr *elf = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
+    // ramdisk_read(elf, 0, sizeof(Elf_Ehdr));
+    // // Check ELF magic number
+    // Log("elf->e_ident = 0x%p", *(uint32_t *)elf->e_ident);
+    // assert(*(uint32_t *)elf->e_ident == 0x464C457F);
+    // // Check ELF machine
+    // Log("elf->e_machine = 0x%p", elf->e_machine);
+    // assert(EXPECT_TYPE == elf->e_machine);
+    // // Program Header
+    // Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf->e_phnum);
+    // ramdisk_read(phdr, elf->e_phoff, sizeof(Elf_Phdr) * elf->e_phnum);
+    // // Analysis of program header table
+    // for (int i = 0; i < elf->e_phnum; ++i) {
+    //     if (phdr[i].p_type != PT_LOAD) continue;
+    //     ramdisk_read((char*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+    //     memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    // }
+    // return elf->e_entry;
+
+  // 1. open elf files, get file id:
+  int fd = fs_open(filename,0,0);
+
+  // 2. read elf head:
+  Elf_Ehdr *elf_head = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
+  if(fs_read(fd,elf_head,sizeof(Elf_Ehdr)) == -1) assert(0);
+  // ramdisk_read(elf_head,0,sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)(elf_head->e_ident) == 0x464c457f);  // check reading file is elf.
+  assert(elf_head->e_machine == EXPECT_TYPE);              // check architecture.
+
+  // 3. read program headers, remeber pro_head is a struct pointer!!
+  Elf_Phdr *pro_head = (Elf_Phdr*)malloc(sizeof(Elf_Phdr)*elf_head->e_phnum);
+  if(fs_read(fd, pro_head,sizeof(Elf_Phdr)*elf_head->e_phnum) == -1)  assert(0);
+  // ramdisk_read((void*)(p->p_vaddr), p->p_offset, p->p_filesz);
+
+  // 4. read text/rodata/data/bss segment to mem:
+  for(Elf_Phdr *p=pro_head; p<pro_head+elf_head->e_phnum; p++){
+
+    // 4.1 load text/rodata/data segment into mem:
+    if(fs_lseek(fd,p->p_offset,SEEK_SET) == -1) assert(0);
+    if(fs_read(fd, (void*)(p->p_vaddr),p->p_filesz) == -1)  assert(0);
+    // ramdisk_read((void*)(p->p_vaddr), p->p_offset, p->p_filesz);
+    
+    // 4.2 init bss segment(set to zero):
+    memset((void *)(p->p_vaddr+p->p_filesz), 0, p->p_memsz - p->p_filesz);
+  }
+
+  free(elf_head);
+  free(pro_head);
+
+  return elf_head->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
+  Log("Jump to entry = %lx", entry);
   ((void(*)())entry) ();
 }
 

a97c61c01d0887b6602e4a6f21051af935473cca
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 22:53:58 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:53:58 up 2 days,  1:43,  1 user,  load average: 1.30, 0.77, 0.62

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -89,6 +89,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 size_t fs_write(int fd, const void *buf, size_t len){
     printf("here\n");
     if(file_table[fd].write != NULL){
+    printf("here\n");
         return (file_table[fd].write)(buf,open_offset,len);
     }
     else{
@@ -101,7 +102,6 @@ size_t fs_write(int fd, const void *buf, size_t len){
         }
         ramdisk_write(buf, file_table[fd].disk_offset + open_offset, len);
         open_offset = open_offset + len;
-        printf("here\n");
         return len;
     }
 }

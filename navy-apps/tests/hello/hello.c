fcaa3f59cbbd21d100801249fa8b80080f3b1dac
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-23 23:47:06 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:47:06 up 2 days,  2:36,  1 user,  load average: 0.85, 0.50, 0.42

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 
 int main() {
-printf("here");
-  write(1, "Hello World!\n", 13);
-  printf("here");
-  int i = 2;
-  volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
+    write(1, "Hello World!\n", 13);
+    write(1, "Hello World!\n", 13);
+    int i = 2;
+    volatile int j = 0;
+    while (1) {
+        j ++;
+        if (j == 10000) {
+        printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+        j = 0;
+        }
     }
-  }
-  return 0;
+    return 0;
 }

38d3fe7fc0ab6d9b683aa9a8fa9ba9856eb01e5c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 01:55:53 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:55:53 up 2 days,  4:45,  1 user,  load average: 0.56, 0.40, 0.48

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -4,10 +4,12 @@
 int main() {
     int i=0;
     printf("RTC begin\n");
-    printf("%d", NDL_GetTicks());
+    printf("%d\n", NDL_GetTicks());
+    printf("%d\n", NDL_GetTicks());
     while(NDL_GetTicks()/500){
         printf("%d time has gone 0.5s\n",i);
         i++;
     }
+    printf("%d\n", NDL_GetTicks());
     return 0;
 }

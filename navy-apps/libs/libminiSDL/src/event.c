9d01cc5b192801c02a786c50b60bd792e9280a17
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 18:45:18 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:45:18 up  3:34,  1 user,  load average: 0.45, 0.64, 0.67

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,6 +14,37 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
+  char buf[100];
+
+  if(NDL_PollEvent(buf,ARRLEN(buf)-1) != 0){
+    if(strncmp (buf, "kd ", 3) == 0){
+      ev->key.type = SDL_KEYDOWN;
+    }
+    else if(strncmp (buf, "ku ", 3) == 0){
+      ev->key.type = SDL_KEYUP;
+    }
+    if(ev->type == SDL_KEYDOWN || ev->type == SDL_KEYUP){
+      //printf("SDL_PollEvent get: %s\n",buf);
+#if !defined(__ISA_NATIVE__)  // direct read keycode.
+      uint8_t keycode = 0;
+      sscanf(buf+3,"%2d %s",&keycode,buf+6);  // buf+3 -> buf+3 means %s do not change.
+      if(keycode != 0){
+        keystate[keycode] = (ev->type == SDL_KEYDOWN) ? 1:0;
+        ev->key.keysym.sym = keycode;
+        return 1;
+      }
+#else // use for native, use keyname to match keycode.
+      buf[strlen(buf)-1] = '\0'; // remove '\n'
+      for(int i=0; i<ARRLEN(keyname); i++){
+        if(strcmp (buf+3, keyname[i]) == 0){
+          ev->key.keysym.sym = i;
+          keystate[i] = (ev->type == SDL_KEYDOWN) ? 1:0; 
+          return 1;
+        }
+      }
+#endif
+    }
+  }
   return 0;
 }
 

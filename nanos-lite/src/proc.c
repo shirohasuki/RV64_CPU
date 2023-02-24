f4bf33ceabe5ac51e89dabf805afbd22cb2906ea
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 15:31:33 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:31:33 up 20 min,  1 user,  load average: 0.73, 0.72, 0.69

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,9 @@ void init_proc() {
     // naive_uload(NULL,"/bin/hello");
     // naive_uload(NULL,"/bin/file-test");
     // naive_uload(NULL,"/bin/timer-test");
-    naive_uload(NULL,"/bin/event-test");
+    // naive_uload(NULL,"/bin/event-test");
+    naive_uload(NULL,"/bin/bmp-test");
+
 
 
 }

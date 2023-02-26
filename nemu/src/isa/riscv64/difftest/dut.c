5ec3b33f1700b5fa2b1d06d1bc6f917cfc7c850d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-26 11:35:42 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:35:42 up  9:56,  1 user,  load average: 0.70, 0.85, 0.66

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -3,16 +3,19 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-    if (cpu.pc != ref_r->pc) return false;
+    if (cpu.pc != ref_r->pc) {
+        printf("[difftest] PC unequal: nemu: %lx native: %lx\n", cpu.pc, ref_r->pc);
+        return false;
+    }
     for (int i = 0; i < 32; i++) {
-        if (gpr(i) != ref_r->gpr[i]) {
-            Log("Reg: %lx %lx", gpr(i), ref_r -> gpr[i]);
+        if (cpu.gpr[i] != ref_r->gpr[i]) {
+            printf("[difftest] REG unequal(pc=%lx): nemu: %lx native: %lx\n", cpu.pc, cpu.gpr[i], ref_r->gpr[i]);
             return false;
         }
     }
     for (int i = 0; i < 4; i++) {
         if (cpu.csr[i] != ref_r->csr[i]) {
-            printf("cpu: %ld  ref:  %ld\n", cpu.csr[i], ref_r->csr[i]);
+            printf("[difftest] CSR unequal(pc=%lx): nemu: %lx native: %lx\n", cpu.pc, cpu.csr[i], ref_r->csr[i]);
             return false;
         }
     }

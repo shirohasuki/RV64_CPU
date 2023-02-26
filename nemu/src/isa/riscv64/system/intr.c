a9e555f697e06b335a37d2c4512d671d568210c0
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-26 12:22:19 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:22:19 up 10:43,  1 user,  load average: 1.16, 1.17, 0.96

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -1,40 +1,19 @@
 #include <isa.h>
-#include "../local-include/reg.h"
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
     // cpu.csr[mepc] = epc;
     // cpu.csr[mcause] = NO;
     // return cpu.csr[mtvec];
     // return 0;
-//     cpu.csr[mepc] = epc;
-//     cpu.csr[mcause] = NO;
-// #ifdef CONFIG_ETRACE
-//     printf("[ETrace] Exception: mtvec = 0x%016lx, mepc = 0x%016lx, mstatus = 0x%016lx, mcause = 0x%016lx\n",
-//         cpu.csr[mtvec], cpu.csr[mepc], cpu.csr[mstatus], cpu.csr[mcause]);
-// #endif
-//     return cpu.csr[mtvec];
-
-// Trigger an interrupt/exception with ``NO'', return the address of the interrupt/exception vector.
+    cpu.csr[mepc] = epc;
+    cpu.csr[mcause] = NO;
 #ifdef CONFIG_ETRACE
-  printf("etrace detect interrupt/exception: mepc==%lx, mcause==%lx, mtvec==%lx\n",epc,NO,csr(mtvec));
+    printf("[ETrace] Exception: mtvec = 0x%016lx, mepc = 0x%016lx, mstatus = 0x%016lx, mcause = 0x%016lx\n",
+        cpu.csr[mtvec], cpu.csr[mepc], cpu.csr[mstatus], cpu.csr[mcause]);
 #endif
-  word_t i_mstatus = csr(mstatus);
-  csr(mstatus) = (SEXT(BITS(i_mstatus, 63, 13), 51) << 13) | (BITS(3, 1, 0) << 11) | (BITS(i_mstatus, 10, 8) << 8) | (BITS(i_mstatus, 3, 3) << 7)  \
-                    | (BITS(i_mstatus, 6, 4) << 4) | (BITS(0, 1, 1) << 3) | (BITS(i_mstatus, 2, 0)) ;
-  //printf("before/after ecall mstatus == %lx, %lx\n", i_mstatus, csr(mstatus));
-  csr(mepc)   = epc;  // PC -> CSR[mepc];
-  csr(mcause) = NO;   // NO -> CSR[mcause];
-  return csr(mtvec);  // CSR[mtvec] -> handler_addr;
+    return cpu.csr[mtvec];
 }
 
 word_t isa_query_intr() {
     return INTR_EMPTY;
-}
-
-word_t isa_mret() {
-  word_t i_mstatus = csr(mstatus);
-  csr(mstatus) = (SEXT(BITS(i_mstatus, 63, 13), 51) << 13) | (BITS(0, 1, 0) << 11) | (BITS(i_mstatus, 10, 8) << 8) | (BITS(1, 0, 0) << 7)  \
-                    | (BITS(i_mstatus, 6, 4) << 4) | (BITS(i_mstatus, 7, 7) << 3) | (BITS(i_mstatus, 2, 0)) ;
-  //printf("before/after mret mstatus == %lx, %lx\n", i_mstatus, csr(mstatus));
-  return csr(mepc); 
 }

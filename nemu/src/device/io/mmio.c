d641ce804bdef2f80f0d97629c03611551da2428
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-02 16:37:04 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:37:04 up 2 days,  5:18,  1 user,  load average: 0.58, 0.29, 0.16

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -23,9 +23,9 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-  return map_read(addr, len, fetch_mmio_map(addr));
+    return map_read(addr, len, fetch_mmio_map(addr));
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
-  map_write(addr, len, data, fetch_mmio_map(addr));
+    map_write(addr, len, data, fetch_mmio_map(addr));
 }

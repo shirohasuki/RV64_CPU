a42defe76b3d3ea3824a612e32837a98c784f721
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-06 16:42:05 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:42:05 up  3:57,  1 user,  load average: 0.92, 0.65, 0.65

--- abstract-machine/am/src/riscv/npc/include/npc.h
+++ abstract-machine/am/src/riscv/npc/include/npc.h
@@ -5,16 +5,18 @@
 #include <riscv/riscv.h>
 
 #define DEVICE_BASE 0xa0000000
-// #define MMIO_BASE   0xa0000000
+#define MMIO_BASE   0xa0000000
 
 #define SERIAL_PORT     (DEVICE_BASE + 0x000003f8)
 #define KBD_ADDR        (DEVICE_BASE + 0x00000060)
 #define RTC_ADDR        (DEVICE_BASE + 0x00000048)
 
+#define FB_ADDR         (MMIO_BASE   + 0x1000000)
+
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-#define NEMU_PADDR_SPACE \
+#define NPC_PADDR_SPACE \
   RANGE(&_pmem_start, PMEM_END), \
   RANGE(FB_ADDR, FB_ADDR + 0x200000), \
   RANGE(MMIO_BASE, MMIO_BASE + 0x1000) /* serial, rtc, screen, keyboard */

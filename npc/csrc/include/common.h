1ec66378eaefa5ce41816bb41787be1011d7382d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-31 14:00:49 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:00:49 up 1 day, 22:13,  1 user,  load average: 0.54, 1.24, 1.03

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -41,7 +41,7 @@ extern NPCState npc_state;
 #define NPC_HAS_SERIAL      1
 #define NPC_HAS_TIMER       1
 // #define NPC_HAS_KBD         1
-// #define NPC_HAS_VGA         1
+#define NPC_HAS_VGA         1
 
 // ============== DEVICE MAP ===============
 #define DEVICE_BASE         0xa0000000

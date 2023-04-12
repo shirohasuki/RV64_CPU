f9df0c78b18895149294352d3d812eddec1b11b8
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 23:25:03 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:25:03 up 1 day,  5:02,  1 user,  load average: 1.42, 1.40, 1.43

--- npc/csrc/src/ioe/io/map.c
+++ npc/csrc/src/ioe/io/map.c
@@ -63,6 +63,6 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
     check_bound(map, addr);
     paddr_t offset = addr - map->low;
     // printf("addr = %lx data = %lx \n", (uint64_t *)map->space + offset, data);
-    host_write((uint64_t *)map->space + offset, len, data);
+    host_write(map->space + offset, len, data);
     invoke_callback(map->callback, offset, len, true);
 }

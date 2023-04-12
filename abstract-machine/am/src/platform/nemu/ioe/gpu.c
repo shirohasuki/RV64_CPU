a177a90d6ed0a53e549ac2089e38c84418d53e4f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 22:27:26 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:27:26 up 1 day,  4:04,  1 user,  load average: 1.29, 1.15, 0.75

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -26,18 +26,18 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 // AM显示控制器信息, 可读出屏幕大小信息width和height
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-    // int height = inw(VGACTL_ADDR);
-    int width = inw(VGACTL_ADDR + 2); // VGACTL_ADDR存H, VGACTL_ADDR + 2存W
-    // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    for (int x = 0; x < ctl->w; x++) {
-        for (int y = 0; y < ctl->h; y++) {
-            // outl(FB_ADDR, 1);
-            outl(FB_ADDR + (width * (ctl->y + y) + (ctl->x + x)) * sizeof(uint32_t), ((uint32_t*)ctl->pixels)[y * ctl->w + x]);
-        }
-    }
-    if (ctl->sync) {
-        // outl(SYNC_ADDR, 1);
-    }
+    // // int height = inw(VGACTL_ADDR);
+    // int width = inw(VGACTL_ADDR + 2); // VGACTL_ADDR存H, VGACTL_ADDR + 2存W
+    // // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    // for (int x = 0; x < ctl->w; x++) {
+    //     for (int y = 0; y < ctl->h; y++) {
+    //         // outl(FB_ADDR, 1);
+    //         outl(FB_ADDR + (width * (ctl->y + y) + (ctl->x + x)) * sizeof(uint32_t), ((uint32_t*)ctl->pixels)[y * ctl->w + x]);
+    //     }
+    // }
+    // if (ctl->sync) {
+    //     outl(SYNC_ADDR, 1);
+    // }
 }
 // AM帧缓冲控制器, 可写入绘图信息, 向屏幕(x, y)坐标处绘制w*h的矩形图像. 
 // 图像像素按行优先方式存储在pixels中, 每个像素用32位整数以00RRGGBB的方式描述颜色. 

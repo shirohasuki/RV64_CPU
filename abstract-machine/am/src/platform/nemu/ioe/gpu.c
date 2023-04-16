6d8aa13eb621af25b442211092754882e5cd7ac4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 15:26:41 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:26:41 up 4 days, 21:03,  1 user,  load average: 1.18, 0.73, 0.70

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,14 +4,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-    int height = inw(VGACTL_ADDR);
-    int width = inw(VGACTL_ADDR + 2);
-    int i;
-    int w = width; 
-    int h = height; 
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    for (i = 0; i < w * h; i ++)  {if (i >= 0 && i <= 400) fb[i] = 199199199; else fb[i] = 0; }
-    outl(SYNC_ADDR, 1);
+    // int height = inw(VGACTL_ADDR);
+    // int width = inw(VGACTL_ADDR + 2);
+    // int i;
+    // int w = width; 
+    // int h = height; 
+    // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    // for (i = 0; i < w * h; i ++)  {if (i >= 0 && i <= 400) fb[i] = 199199199; else fb[i] = 0; }
+    // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {

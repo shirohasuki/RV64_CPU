8d64325ceb6a7fa832945faae63413783c5ca367
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 21:10:30 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:10:30 up  1:08,  1 user,  load average: 1.96, 1.20, 1.26

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -12,7 +12,7 @@
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
     word_t mstatus_buf   = 0; mstatus_buf = cpu.csr[mstatus];
     cpu.csr[mstatus] = (SEXT(BITS(mstatus_buf, 63, 13), 51) << 13) | (BITS(3, 1, 0) << 11) | (BITS(mstatus_buf, 10, 8) << 8) | (BITS(mstatus_buf, 3, 3) << 7) | (BITS(mstatus_buf, 6, 4) << 4) | (BITS(0, 1, 1) << 3) | (BITS(mstatus_buf, 2, 0)) ;
-    cpu.csr[2]    = epc; 
+    cpu.csr[0]    = epc; 
     cpu.csr[mcause]  = NO;  
 #ifdef CONFIG_ETRACE
     printf("[ETrace] Exception: mtvec = 0x%016lx, mepc = 0x%016lx, mstatus = 0x%016lx, mcause = 0x%016lx\n", cpu.csr[mtvec], cpu.csr[mepc], cpu.csr[mstatus], cpu.csr[mcause]);

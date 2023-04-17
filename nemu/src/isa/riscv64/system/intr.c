14fb782361431e3060abc10c88e350ca69419183
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 21:09:39 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:09:39 up  1:07,  1 user,  load average: 1.81, 1.06, 1.22

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -10,10 +10,10 @@
 // }
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-    word_t mstatus = 0; mstatus = cpu.csr[mstatus];
-    cpu.csr[mstatus] = (SEXT(BITS(mstatus, 63, 13), 51) << 13) | (BITS(3, 1, 0) << 11) | (BITS(mstatus, 10, 8) << 8) | (BITS(mstatus, 3, 3) << 7) | (BITS(mstatus, 6, 4) << 4) | (BITS(0, 1, 1) << 3) | (BITS(mstatus, 2, 0)) ;
-    cpu.csr[mepc]   = epc; 
-    cpu.csr[mcause] = NO;  
+    word_t mstatus_buf   = 0; mstatus_buf = cpu.csr[mstatus];
+    cpu.csr[mstatus] = (SEXT(BITS(mstatus_buf, 63, 13), 51) << 13) | (BITS(3, 1, 0) << 11) | (BITS(mstatus_buf, 10, 8) << 8) | (BITS(mstatus_buf, 3, 3) << 7) | (BITS(mstatus_buf, 6, 4) << 4) | (BITS(0, 1, 1) << 3) | (BITS(mstatus_buf, 2, 0)) ;
+    cpu.csr[mepc]    = epc; 
+    cpu.csr[mcause]  = NO;  
 #ifdef CONFIG_ETRACE
     printf("[ETrace] Exception: mtvec = 0x%016lx, mepc = 0x%016lx, mstatus = 0x%016lx, mcause = 0x%016lx\n", cpu.csr[mtvec], cpu.csr[mepc], cpu.csr[mstatus], cpu.csr[mcause]);
 #endif
@@ -21,8 +21,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_mret() {
-    word_t mstatus = 0; mstatus = cpu.csr[mstatus];
-    cpu.csr[mstatus] = (SEXT(BITS(mstatus, 63, 13), 51) << 13) | (BITS(0, 1, 0) << 11) | (BITS(mstatus, 10, 8) << 8) | (BITS(1, 0, 0) << 7) | (BITS(mstatus, 6, 4) << 4) | (BITS(mstatus, 7, 7) << 3) | (BITS(mstatus, 2, 0)) ;
+    word_t mstatus_buf   = 0; mstatus_buf = cpu.csr[mstatus];
+    cpu.csr[mstatus] = (SEXT(BITS(mstatus_buf, 63, 13), 51) << 13) | (BITS(0, 1, 0) << 11) | (BITS(mstatus_buf, 10, 8) << 8) | (BITS(1, 0, 0) << 7) | (BITS(mstatus_buf, 6, 4) << 4) | (BITS(mstatus_buf, 7, 7) << 3) | (BITS(mstatus_buf, 2, 0)) ;
     return cpu.csr[mepc]; 
 }
 

8493aa132bc790af85422591dd5b350c96d37b19
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-19 05:54:46 +0800

    > run NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:54:46 up  7:25,  1 user,  load average: 1.05, 0.98, 0.97

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -24,7 +24,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 word_t isa_mret() {
     word_t mstatus_buf   = 0; mstatus_buf = cpu.csr[mstatus];
     cpu.csr[mstatus] = (SEXT(BITS(mstatus_buf, 63, 13), 51) << 13) | (BITS(0, 1, 0) << 11) | (BITS(mstatus_buf, 10, 8) << 8) | (BITS(1, 0, 0) << 7) | (BITS(mstatus_buf, 6, 4) << 4) | (BITS(mstatus_buf, 7, 7) << 3) | (BITS(mstatus_buf, 2, 0)) ;
-    return cpu.csr[mepc] + 4; 
+    cpu.csr[mepc] += 4;
+    return cpu.csr[mepc]; 
 }
 
 

7b3b86dc6b05a9d53a4bba8617f2bc0cd419db58
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-06 23:08:46 +0800

    > compile NEMU ysyx_22040058 王晖 Linux shiroha 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:08:46 up 11:05,  1 user,  load average: 2.04, 1.35, 1.28

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -25,9 +25,9 @@ static char* rl_gets() {
 
     line_read = readline("(nemu) ");
 
-    // if (line_read && *line_read) {
-    //   	add_history(line_read);
-    // }
+    if (line_read && *line_read) {
+      	add_history(line_read);
+    }
 
     return line_read;
 }

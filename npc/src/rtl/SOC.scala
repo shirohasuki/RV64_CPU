aeeb81e4acb6aad7c3302619d3491b20ff5ceecb
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-20 15:11:54 +0800

    > sim RTL ysyx_22040058 王晖 Linux shiroha 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:11:54 up  1:43,  1 user,  load average: 1.78, 1.42, 1.57

--- npc/src/rtl/SOC.scala
+++ npc/src/rtl/SOC.scala
@@ -2,7 +2,7 @@ import chisel3._
 import chisel3.util._
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
-class Passthrough extends Module {
+class tb extends Module {
     val io = IO(new Bundle {
         val in = Input(UInt(4.W))
         val out = Output(UInt(4.W))

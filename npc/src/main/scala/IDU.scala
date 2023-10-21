457e2adf8b2b0229d0cf2dc6d55f165f829c8c65
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-22 00:07:23 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:07:23 up  5:06,  1 user,  load average: 1.79, 1.63, 1.62

--- npc/src/main/scala/IDU.scala
+++ npc/src/main/scala/IDU.scala
@@ -68,7 +68,7 @@ class IDU extends Module {
     io.id_ex.pc_id_ex   := io.if_id.pc_if_id    
     val func37 = Cat(func3, func7)
     //  List(op1_o, op2_o, rs1_addr_o, rs2_addr_o, rd_addr_o, reg_wen, base_addr_o, offset_addr_o)
-    var id_ex_list  = ListLookup(func37, List((0.U(64.W)), 0.U(64.W), 0.U(5.W), 0.U(5.W), 0.U(64.W), false.B, 0.U(64.W), 0.U(64.W)), Array(
+    var id_ex_list  = ListLookup(func37, List(0.U(64.W), 0.U(64.W), 0.U(5.W), 0.U(5.W), 0.U(64.W), false.B, 0.U(64.W), 0.U(64.W)), Array(
         BitPat("000_0010011") -> List(io.reg_id.rs1_data_reg_id, immI,                      rs1_addr,       0.U,          rd_addr,    1.B,   0.U,   0.U), //addi
         BitPat("000_0110011") -> List(io.reg_id.rs1_data_reg_id, io.reg_id.rs2_data_reg_id, rs1_addr,       rs2_addr,     rd_addr,    1.B,   0.U,   0.U), //add        
     ))

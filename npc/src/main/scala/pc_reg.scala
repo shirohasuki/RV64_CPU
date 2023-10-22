fc64f17e0f17e218200da598e02fa039a6458a48
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-22 13:58:07 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:58:07 up 18:57,  1 user,  load average: 0.64, 0.93, 0.82

--- npc/src/main/scala/pc_reg.scala
+++ npc/src/main/scala/pc_reg.scala
@@ -6,12 +6,12 @@ import chisel3.stage._
 
 class PcReg extends Module {
     val pc_if = IO(new Bundle {
-        val pc = Output(UInt(64.W))
+        val pc_pc_if = Output(UInt(64.W))
     })
 
-    val pc   = RegInit("h80000000".U(64.W))
-    val snpc = Reg(UInt(64.W))
+    val pc_pc_if   = RegInit("h80000000".U(64.W))
+    val snpc_pc_if = Reg(UInt(64.W))
     // val pc   = Reg(Uint(64.W))
-    pc   := snpc
-    snpc := pc + 4.U 
+    pc_pc_if   := snpc_pc_if
+    snpc_pc_if := pc_pc_if + 4.U 
 }

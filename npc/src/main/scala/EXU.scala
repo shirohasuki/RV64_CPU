0385fba4726e92d16ff271c73a000e98ae91e074
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-22 15:10:04 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:10:04 up 20:09,  1 user,  load average: 2.36, 1.72, 1.49

--- npc/src/main/scala/EXU.scala
+++ npc/src/main/scala/EXU.scala
@@ -29,7 +29,7 @@ class EXU extends Module {
     val ex_reg = IO(new EXU_REG_Output())
     // })
     
-    reg op1_i_add_op2_i =  id_ex.op1 + id_ex.op2
+    val op1_i_add_op2_i =  Reg(id_ex.op1 + id_ex.op2)
 
     // def op1_i_add_op2_i
     

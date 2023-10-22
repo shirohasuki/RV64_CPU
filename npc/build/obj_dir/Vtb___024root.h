07e9dad9bb9b4ce7a1066125ab238afbd007392d
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-22 18:42:03 +0800

    > sim RTL ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:42:03 up 23:41,  1 user,  load average: 1.86, 1.48, 1.43

---
+++ npc/build/obj_dir/Vtb___024root.h
@@ -0,0 +1,31 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vtb.h for the primary calling header
+
+#ifndef VERILATED_VTB___024ROOT_H_
+#define VERILATED_VTB___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+class Vtb__Syms;
+VL_MODULE(Vtb___024root) {
+  public:
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clock,0,0);
+    VL_IN8(reset,0,0);
+
+    // INTERNAL VARIABLES
+    Vtb__Syms* vlSymsp;  // Symbol table
+
+    // CONSTRUCTORS
+    Vtb___024root(const char* name);
+    ~Vtb___024root();
+    VL_UNCOPYABLE(Vtb___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(Vtb__Syms* symsp, bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+
+#endif  // guard

57b6c001ce52016866aed2eb106abef4df7c5ee8
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-23 19:02:08 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:02:08 up 2 days, 1 min,  1 user,  load average: 2.33, 2.65, 2.43

--- npc/src/main/scala/IDEX.scala
+++ npc/src/main/scala/IDEX.scala
@@ -16,7 +16,7 @@ class IDEX_Input extends Bundle {
 
 class IDEX extends Module {  
     val id_idex = IO(new IDEX_Input())
-    val idex_ex = IO(Flipped(new IDEX_Input()))
+    val idex_ex = IO(Output(Flipped(new IDEX_Input())))
 
     def dff_set(flush_flag_i: UInt, stall_flag_i: UInt, DataWidth: UInt, default: UInt, data_i: UInt): UInt = {
         val data_o = RegInit(default)
@@ -27,11 +27,11 @@ class IDEX extends Module {
 
     idex_ex.inst        := dff_set(0.U, 0.U, 32.U,  "h00000013".U, id_idex.inst       )
     idex_ex.pc          := dff_set(0.U, 0.U, 64.U,  0.U,           id_idex.pc         )
-    idex_ex.op1         := dff_set(0.U, 0.U, 64.U, 0.U,           id_idex.op1        )
-    idex_ex.op2         := dff_set(0.U, 0.U, 64.U, 0.U,           id_idex.op2        )
-    idex_ex.rd_addr     := dff_set(0.U, 0.U, 5.U,  0.U,           id_idex.rd_addr    )
-    idex_ex.rd_wen      := dff_set(0.U, 0.U, 1.U,  0.U,           id_idex.rd_wen     )
-    idex_ex.base_addr   := dff_set(0.U, 0.U, 64.U, 0.U,           id_idex.base_addr  )
-    idex_ex.offset_addr := dff_set(0.U, 0.U, 64.U, 0.U,           id_idex.offset_addr)
+    idex_ex.op1         := dff_set(0.U, 0.U, 64.U,  0.U,           id_idex.op1        )
+    idex_ex.op2         := dff_set(0.U, 0.U, 64.U,  0.U,           id_idex.op2        )
+    idex_ex.rd_addr     := dff_set(0.U, 0.U, 5.U,   0.U,           id_idex.rd_addr    )
+    idex_ex.rd_wen      := dff_set(0.U, 0.U, 1.U,   0.U,           id_idex.rd_wen     )
+    idex_ex.base_addr   := dff_set(0.U, 0.U, 64.U,  0.U,           id_idex.base_addr  )
+    idex_ex.offset_addr := dff_set(0.U, 0.U, 64.U,  0.U,           id_idex.offset_addr)
 
 }

44af001c2f8c7ff0da918a9e6022874321620331
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-27 01:24:11 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:24:11 up 3 days,  2:50,  1 user,  load average: 1.75, 1.11, 0.94

--- npc/src/main/scala/CTRL.scala
+++ npc/src/main/scala/CTRL.scala
@@ -85,9 +85,9 @@ class Ctrl extends Module {
 
     //  List(pc_stall_en, if_id_stall_en, id_ex_stall_en, ex_mem_stall_en, mem_wb_stall_en)
     val stall_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B, false.B), Array(
-        BitPat(1.U) -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isload,  false.B), // load_inst   
-        BitPat(2.U) -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isstore, false.B), // store_inst         
-        BitPat(3.U) -> List(true.B, true.B, false.B, false.B, false.B)                   // load_data_hit      
+        BitPat(1.U) -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isload,  false.B),   // load_inst   
+        BitPat(2.U) -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isstore, false.B),   // store_inst         
+        BitPat(3.U) -> List(true.B, true.B, false.B, false.B, false.B)                      // load_data_hit      
     ))
 
     ctrl_pc.pc_stall_en         := stall_list(0)
@@ -98,11 +98,11 @@ class Ctrl extends Module {
 
         //  List(pc_flush_en, if_id_flush_en, id_ex_flush_en, ex_mem_flush_en, mem_wb_flush_en)
     val flush_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B, false.B), Array(
-        BitPat(0.U) -> List(false.B, false.B,  true.B, false.B, false.B), // jump
-        BitPat(1.U) -> List(false.B, false.B, true.B, false.B, false.B), // load_inst          
-        BitPat(2.U) -> List(false.B, false.B, true.B, false.B, false.B), // store_inst          
-        BitPat(3.U) -> List(false.B, false.B, true.B, false.B, false.B), // load_data_hit 
-        BitPat(4.U) -> List(false.B, false.B, false.B, false.B, false.B) // load_data_hit 
+        BitPat("b000".U) -> List(false.B, true.B,  true.B, false.B, false.B),    // jump
+        BitPat(001.U) -> List(false.B, false.B, true.B, false.B, false.B),    // load_inst          
+        BitPat(002.U) -> List(false.B, false.B, true.B, false.B, false.B),    // store_inst          
+        BitPat(003.U) -> List(false.B, false.B, true.B, false.B, false.B),    // load_data_hit 
+        // BitPat(4.U) -> List(false.B, false.B, false.B, false.B, false.B) // Noevent
     ))
 
     ctrl_pc.pc_flush_en         := flush_list(0)

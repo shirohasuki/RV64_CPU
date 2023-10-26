adcc943f8c6066027b340b039f5604b75fbc7537
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-26 22:45:08 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:45:08 up 3 days, 11 min,  1 user,  load average: 1.47, 1.52, 1.37

--- npc/src/main/scala/Redirect.scala
+++ npc/src/main/scala/Redirect.scala
@@ -68,16 +68,17 @@ class Redirect extends Module {
     redirect_ctrl.rs_id_ex_hit := rs1_id_ex_hit || rs2_id_ex_hit
 
     // rs1_addr_i数据冲突 reg_redirect.rs1_rdata为默认值
-    redirect_id.rs1_rdata := MuxCase(reg_redirect.rs1_rdata, ArraySeq.unsafeWrapArray(Array(
+    redirect_id.rs1_rdata := MuxCase(reg_redirect.rs1_rdata, Array(
         rs1_id_ex_hit  ->  ex_redirect.rd_wdata,
         rs1_id_mem_hit ->  mem_redirect.rd_wdata,
         rs1_id_wb_hit  ->  wb_redirect.rd_wdata
-    )))
+    ).mapValues(ArraySeq.unsafeWrapArray))
 
     // rs2_addr_i数据冲突 reg_redirect.rs2_rdata为默认值
-    redirect_id.rs2_rdata := MuxCase(reg_redirect.rs2_rdata, ArraySeq.unsafeWrapArray(Array(
+    redirect_id.rs2_rdata := MuxCase(reg_redirect.rs2_rdata, Array(
         rs2_id_ex_hit   -> ex_redirect.rd_wdata,
         rs2_id_mem_hit  -> mem_redirect.rd_wdata,
         rs2_id_wb_hit   -> wb_redirect.rd_wdata
-    )))
+    ).mapValues(ArraySeq.unsafeWrapArray))
 }
+

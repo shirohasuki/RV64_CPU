18a10d824f66a2521cafd4a66df6a580f18a0694
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-26 21:04:08 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:04:08 up 2 days, 22:30,  1 user,  load average: 1.55, 1.02, 1.06

--- npc/src/main/scala/EXU.scala
+++ npc/src/main/scala/EXU.scala
@@ -171,8 +171,8 @@ class EXU extends Module {
                             INST_BLTU       -> List(NORd_Write, 0.U(64.W), 0.U(64.W), NOMEM_Read, 0.U(64.W), NOMEM_Write, 0.U(8.W), 0.U(64.W), 0.U(64.W),  less_unsigned(op1, op2), add(base_addr, offset_addr)),
                             INST_BGE        -> List(NORd_Write, 0.U(64.W), 0.U(64.W), NOMEM_Read, 0.U(64.W), NOMEM_Write, 0.U(8.W), 0.U(64.W), 0.U(64.W),   ~less_signed(op1, op2), add(base_addr, offset_addr)),
                             INST_BGEU       -> List(NORd_Write, 0.U(64.W), 0.U(64.W), NOMEM_Read, 0.U(64.W), NOMEM_Write, 0.U(8.W), 0.U(64.W), 0.U(64.W), ~less_unsigned(op1, op2), add(base_addr, offset_addr)))),
-        INST_TYPE_L     -> ListLookup(idex_ex.func3, default_exce_list, Array( // EXU不提供wdata
-                            INST_LB, INST_LH, INST_LW, INST_LD,INST_LBU, INST_LHU, INST_LWU -> List(Rd_Write, rd_addr, 0.U(64.W), MEM_Read, add(base_addr, offset_addr), NOMEM_Write, 0.U(8.W), 0.U(64.W), 0.U(64.W), NOTypeJ_Jump, 0.U(64.W)))),
+        INST_TYPE_L     -> //ListLookup(idex_ex.func3, default_exce_list, Array( // EXU不提供wdata
+                            INST_LB, INST_LH, INST_LW, INST_LD,INST_LBU, INST_LHU, INST_LWU -> List(Rd_Write, rd_addr, 0.U(64.W), MEM_Read, add(base_addr, offset_addr), NOMEM_Write, 0.U(8.W), 0.U(64.W), 0.U(64.W), NOTypeJ_Jump, 0.U(64.W)),//)),
         INST_TYPE_S     -> ListLookup(idex_ex.func3, default_exce_list, Array(
                             INST_SB         -> List(NORd_Write, 0.U(64.W), 0.U(64.W), MEM_Read, add(base_addr, offset_addr), MEM_Write, "b00000001".U, Cat(Fill(56, 0.U), op2(7,  0)), add(base_addr, offset_addr), NOTypeJ_Jump, 0.U(64.W)),         
                             INST_SH         -> List(NORd_Write, 0.U(64.W), 0.U(64.W), MEM_Read, add(base_addr, offset_addr), MEM_Write, "b00000011".U, Cat(Fill(48, 0.U), op2(15, 0)), add(base_addr, offset_addr), NOTypeJ_Jump, 0.U(64.W)), 

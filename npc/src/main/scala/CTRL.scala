65298feb323169df86c27b627a651528a9bdf701
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-26 20:50:54 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:50:54 up 2 days, 22:17,  1 user,  load average: 1.03, 1.11, 1.20

--- npc/src/main/scala/CTRL.scala
+++ npc/src/main/scala/CTRL.scala
@@ -80,18 +80,17 @@ class Ctrl extends Module {
     ctrl_pc.jump_en   := jump
 
     // 给事件进行优先编码
-    val event_code = VecInit(jump, load_inst, store_inst, load_data_hit)
-    val event_code_OH = PriorityEncoder(event_code)
+    val event_code = PriorityEncoder(VecInit(jump, load_inst, store_inst, load_data_hit)) // 
 
     //  List(pc_stall_en, if_id_stall_en, id_ex_stall_en, ex_mem_stall_en, mem_wb_stall_en)
-    val stall_list  = ListLookup(event_code_OH, List(false.B, false.B, false.B, false.B, false.B), Array(
-        BitPat("b01") -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isload,  false.B), // load_inst   
-        BitPat("b10") -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isstore, false.B), // store_inst         
-        BitPat("b11") -> List(true.B, true.B, false.B, false.B, false.B)                   // load_data_hit      
+    val stall_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B, false.B), Array(
+        BitPat(1) -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isload,  false.B), // load_inst   
+        BitPat(2) -> List(true.B, true.B, false.B, mem_ctrl.mem_inst_isstore, false.B), // store_inst         
+        BitPat(3) -> List(true.B, true.B, false.B, false.B, false.B)                   // load_data_hit      
     ))
 
         //  List(pc_flush_en, if_id_flush_en, id_ex_flush_en, ex_mem_flush_en, mem_wb_flush_en)
-    val flush_list  = ListLookup(event_code_OH, List(false.B, false.B, false.B, false.B, false.B), Array(
+    val flush_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B, false.B), Array(
         BitPat("b00") -> List(false.B, true.B,  true.B, false.B, false.B), // jump
         BitPat("b01") -> List(false.B, false.B, true.B, false.B, false.B), // load_inst          
         BitPat("b10") -> List(false.B, false.B, true.B, false.B, false.B), // store_inst          

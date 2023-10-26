e653e2ec3627d9e210603fbe8a33993588d44244
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-27 00:09:59 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:09:59 up 3 days,  1:36,  1 user,  load average: 2.48, 1.77, 1.24

--- npc/src/main/scala/CTRL.scala
+++ npc/src/main/scala/CTRL.scala
@@ -81,7 +81,7 @@ class Ctrl extends Module {
 
     // 给事件进行优先编码
     val event_code = RegInit(0.U(2.W)) 
-    event_code := PriorityEncoder(VecInit(jump, load_inst, store_inst, load_data_hit)) // 从低到高输出第一个有1的位数
+    event_code := PriorityEncoder(VecInit(load_inst, store_inst, load_data_hit,jump)) // 从低到高输出第一个有1的位数
 
     //  List(pc_stall_en, if_id_stall_en, id_ex_stall_en, ex_mem_stall_en, mem_wb_stall_en)
     val stall_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B, false.B), Array(

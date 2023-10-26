6eef09f1dacf2a0ff62eed65fabcaaa0962301f2
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-26 18:34:15 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:34:15 up 2 days, 20:00,  1 user,  load average: 0.24, 0.20, 0.24

--- npc/src/main/scala/IDU.scala
+++ npc/src/main/scala/IDU.scala
@@ -76,12 +76,7 @@ class IDU extends Module {
     val default_decode_list = List(0.U(64.W), 0.U(64.W), 0.U(5.W), 0.U(5.W), 0.U(64.W), false.B, 0.U(64.W), 0.U(64.W))
     val decode_list  = ListLookup(opcode, default_decode_list, Array(
         INST_TYPE_I     -> ListLookup(func3, default_decode_list, Array(
-                            INST_ADDI-> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U), 
-                            INST_SLTI-> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U), 
-                            INST_SLTIU-> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U), 
-                            INST_XORI-> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U), 
-                            INST_ORI-> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U), 
-                            INST_ANDI -> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U), 
+                            INST_ADDI, INST_SLTI, INST_SLTIU, INST_XORI, INST_ORI, INST_ANDI -> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U), 
                             INST_SLLI, INST_SRI                                              -> List(redirect_id.rs1_rdata, Cat(Fill(58, 0.U), shamt), rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U)))
         INST_TYPE_I_W   -> ListLookup(func3, default_decode_list, Array(
                             INST_ADDIW                                                       -> List(redirect_id.rs1_rdata, immI, rs1_addr, 0.U, rd_addr, true.B, 0.U, 0.U),  

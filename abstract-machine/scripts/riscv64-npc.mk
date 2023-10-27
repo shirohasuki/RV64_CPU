3eeb215695ddf3e9128b16fb24682868d50bb3ff
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-27 15:18:43 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:18:43 up 3 days, 16:45,  1 user,  load average: 1.25, 1.75, 1.68

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -26,16 +26,16 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 
-# NPC_HOME = 
+
 
 run: image
 	@echo '======================= copy .bin -> .hex start ================================='
-	cp $(IMAGE).bin $(NPC_HOME)/image.bin
-	hexdump -v -e'/4 "%08x\n"'  $(NPC_HOME)/image.bin  > $(NPC_HOME)/image.hex
+	@cp $(IMAGE).bin $(NPC_HOME)/image.bin
+	@hexdump -v -e'/4 "%08x\n"'  $(NPC_HOME)/image.bin  > $(NPC_HOME)/image.hex
 #先把bin文件粘贴到npc目录下，后将bin文件转化为HEX文件
-	awk 'getline i{print i}1' $(NPC_HOME)/image.hex > $(NPC_HOME)/image.txt  
+	@awk 'getline i{print i}1' $(NPC_HOME)/image.hex > $(NPC_HOME)/image.txt  
 #交换每两行顺序（读取入ram的顺序）参考：http://blog.chinaunix.net/uid-10540984-id-2427107.html  Tim大师的思路，真是佩服到不行啊，把getline作为pattern，如果是最后一行是奇数行，没有下读取的话，是不会执行后面的print的，完美解决了奇数行最后一行打印的问题，第一行，读取下一行给i，然后打印i的值（即下一行的内容），然后patter为1执行默认的{print}，这样刚好把两行互换，而且还具备了最后一行是奇数行的判断，佩服。
-	sed '$!N;s/\n//g' $(NPC_HOME)/image.txt  > $(NPC_HOME)/image.hex   
+	@sed '$!N;s/\n//g' $(NPC_HOME)/image.txt  > $(NPC_HOME)/image.hex   
 #因为位宽为64所以使用sed两行合并为一行
 	@echo '======================= copy .bin -> .hex finish ================================='
 

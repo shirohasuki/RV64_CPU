a90decd0c424d5330fc9c67b5bf26b8b3f85baad
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-27 14:41:37 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:41:37 up 3 days, 16:08,  1 user,  load average: 1.16, 1.03, 1.04

--- npc/src/csrc/src/main.cpp
+++ npc/src/csrc/src/main.cpp
@@ -78,17 +78,17 @@ int main() {
 //     IFDEF(CONFIG_NPC_DIFFTEST, init_difftest("/home/shiroha/Code/ysyx/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so", img_size););
 
     while (sim_time < MAX_SIM_TIME) {
-        // dump_gpr(); // 打印通用寄存器
-        // dump_csr(); // 打印异常寄存器
+        dump_gpr(); // 打印通用寄存器
+        dump_csr(); // 打印异常寄存器
         exec_once();
         // IFDEF(CONFIG_NPC_DEVICE, device_update());
         // IFDEF(CONFIG_NPC_MTRACE, print_mtrace());
-// #ifdef CONFIG_NPC_DIFFTEST
-//         while (cpu_npc.pc == 0x0) {
-//             exec_once();   
-//         } // EX被冲刷以后，pc再走几拍
-//         difftest_exec_once();
-// #endif
+#ifdef CONFIG_NPC_DIFFTEST
+        while (cpu_npc.pc == 0x0) {
+            exec_once();   
+        } // EX被冲刷以后，pc再走几拍
+        difftest_exec_once();
+#endif
     }
 
     sim_exit();

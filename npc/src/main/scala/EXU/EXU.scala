363b62d5ca008bbec0ba61e4e54b4f287e1760c4
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-02 23:35:39 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:35:39 up  1:50,  1 user,  load average: 0.91, 1.05, 1.19

--- npc/src/main/scala/EXU/EXU.scala
+++ npc/src/main/scala/EXU/EXU.scala
@@ -22,20 +22,6 @@ class IDEX_EXU_Input extends Bundle {
     val func7       = Input(UInt(7.W))
 }
 
-class EXU_ALU_Output extends Bundle {
-    val inst        = Output(UInt(32.W))
-    val pc          = Output(UInt(64.W))
-    val op1         = Output(UInt(64.W))
-    val op2         = Output(UInt(64.W))
-    val rd_addr     = Output(UInt(5.W))
-    val rd_wen      = Output(Bool())
-    val base_addr   = Output(UInt(64.W))
-    val offset_addr = Output(UInt(64.W))
-    val opcode      = Output(UInt(7.W))
-    val func3       = Output(UInt(3.W))
-    val func7       = Output(UInt(7.W))
-}
-
 class ALU_EXU_Input extends Bundle {
     val rd_wen          = Input(Bool()) 
     val rd_waddr        = Input(UInt(5.W))
@@ -73,18 +59,18 @@ class EXU_Redirect_Output extends Bundle {
 }
 
 class EXU extends Module {
-    val idex_ex     = IO(new IDEX_EXU_Input())
+    val idex_ex     = IO(new IDEX_EXU_Input())  
     // val ex_al       = IO(Flipped(new IDEX_EXU_Input()))  // flip之后直接给alu
-    val ex_al       = IO(new EXU_ALU_Output())  
-    val al_ex       = IO(new ALU_EXU_Input())
+    // val ex_al       = IO(new EXU_ALU_Output())  
+    // val al_ex       = IO(new ALU_EXU_Input())
     val ls_ex       = IO(new LSU_EXU_Input())
     val ex_exwb     = IO(new EXU_EXWB_Output())
     val ex_ctrl     = IO(new EXU_CTRL_Output())
     val ex_redirect = IO(new EXU_Redirect_Output())
     
     val ALU = Module(new ALU())
-    idex_ex       <>  ALU.ex_al
-    ALU.al_ex   <>  al_ex
+    idex_ex       <>  ALU.ex_al // ???,奇了怪了怎么相同方向信号反而又可以了,我觉得可能是内部子单元连线方向一致
+    // ALU.al_ex   <>  al_ex
 
     val LSU = Module(new LSU())
     ALU.al_ls   <>  LSU.al_ls
@@ -92,19 +78,19 @@ class EXU extends Module {
 
     // ex to exwb 
     ex_exwb.pc          := idex_ex.pc
-    ex_exwb.rd_wen      := Mux(al_ex.inst_isload | al_ex.inst_isstore, ls_ex.rd_wen  , al_ex.rd_wen  )
-    ex_exwb.rd_waddr    := Mux(al_ex.inst_isload | al_ex.inst_isstore, ls_ex.rd_waddr, al_ex.rd_waddr)
-    ex_exwb.rd_wdata    := Mux(al_ex.inst_isload | al_ex.inst_isstore, ls_ex.rd_wdata, al_ex.rd_wdata)
+    ex_exwb.rd_wen      := Mux(ALU.al_ex.inst_isload | ALU.al_ex.inst_isstore, ls_ex.rd_wen  , ALU.al_ex.rd_wen  )
+    ex_exwb.rd_waddr    := Mux(ALU.al_ex.inst_isload | ALU.al_ex.inst_isstore, ls_ex.rd_waddr, ALU.al_ex.rd_waddr)
+    ex_exwb.rd_wdata    := Mux(ALU.al_ex.inst_isload | ALU.al_ex.inst_isstore, ls_ex.rd_wdata, ALU.al_ex.rd_wdata)
 
 
     // ex to redirect
-    ex_redirect.rd_wen      := Mux(al_ex.inst_isload | al_ex.inst_isstore, ls_ex.rd_wen, al_ex.rd_wen)
-    ex_redirect.rd_waddr    := Mux(al_ex.inst_isload | al_ex.inst_isstore, ls_ex.rd_waddr, al_ex.rd_waddr)
-    ex_redirect.rd_wdata    := Mux(al_ex.inst_isload | al_ex.inst_isstore, ls_ex.rd_wdata, al_ex.rd_wdata)
+    ex_redirect.rd_wen      := Mux(ALU.al_ex.inst_isload | ALU.al_ex.inst_isstore, ls_ex.rd_wen,   ALU.al_ex.rd_wen)
+    ex_redirect.rd_waddr    := Mux(ALU.al_ex.inst_isload | ALU.al_ex.inst_isstore, ls_ex.rd_waddr, ALU.al_ex.rd_waddr)
+    ex_redirect.rd_wdata    := Mux(ALU.al_ex.inst_isload | ALU.al_ex.inst_isstore, ls_ex.rd_wdata, ALU.al_ex.rd_wdata)
     
     // ex to ctrl 
-    ex_ctrl.inst_isload      := al_ex.inst_isload
-    ex_ctrl.inst_isstore     := al_ex.inst_isstore
-    ex_ctrl.typej_jump_en    := al_ex.typej_jump_en
-    ex_ctrl.typej_jump_addr  := al_ex.typej_jump_addr
+    ex_ctrl.inst_isload      := ALU.al_ex.inst_isload
+    ex_ctrl.inst_isstore     := ALU.al_ex.inst_isstore
+    ex_ctrl.typej_jump_en    := ALU.al_ex.typej_jump_en
+    ex_ctrl.typej_jump_addr  := ALU.al_ex.typej_jump_addr
 }

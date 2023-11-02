71710fe2a26ce1e9fde92c0c9b328e5b5fa6f2a9
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-02 14:50:37 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:50:37 up 2 days, 22:32,  1 user,  load average: 2.26, 2.36, 2.16

--- npc/src/main/scala/EXWB.scala
+++ npc/src/main/scala/EXWB.scala
@@ -21,9 +21,9 @@ class EXWB extends Module {
     val exwb_wb   = IO(Flipped(new EXU_WBU_Input()))
     val ctrl_exwb = IO(new Ctrl_Input())
 
-    exwb_wb.pc       := dff_set(ctrl_lswb.lswb_flush_en, ctrl_lswb.lswb_stall_en, 64.U, 0.U(64.W), ex_exwb.pc      )
-    exwb_wb.rd_wdata := dff_set(ctrl_lswb.lswb_flush_en, ctrl_lswb.lswb_stall_en, 64.U, 0.U(64.W), ex_exwb.rd_wdata)
-    exwb_wb.rd_waddr := dff_set(ctrl_lswb.lswb_flush_en, ctrl_lswb.lswb_stall_en, 1.U,  0.U(64.W), ex_exwb.rd_waddr)
-    exwb_wb.rd_wen   := dff_set(ctrl_lswb.lswb_flush_en, ctrl_lswb.lswb_stall_en, 64.U, false.B,   ex_exwb.rd_wen  )
+    exwb_wb.pc       := dff_set(ctrl_exwb.exwb_flush_en, ctrl_exwb.exwb_stall_en, 64.U, 0.U(64.W), ex_exwb.pc      )
+    exwb_wb.rd_wdata := dff_set(ctrl_exwb.exwb_flush_en, ctrl_exwb.exwb_stall_en, 64.U, 0.U(64.W), ex_exwb.rd_wdata)
+    exwb_wb.rd_waddr := dff_set(ctrl_exwb.exwb_flush_en, ctrl_exwb.exwb_stall_en, 1.U,  0.U(64.W), ex_exwb.rd_waddr)
+    exwb_wb.rd_wen   := dff_set(ctrl_exwb.exwb_flush_en, ctrl_exwb.exwb_stall_en, 64.U, false.B,   ex_exwb.rd_wen  )
 }
 

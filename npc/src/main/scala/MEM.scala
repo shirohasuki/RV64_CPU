56ccfe5aa36f958d31d78654f24b81fe4fdc41f8
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-02 20:09:18 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:09:18 up 17 min,  1 user,  load average: 1.12, 1.64, 1.46

--- npc/src/main/scala/MEM.scala
+++ npc/src/main/scala/MEM.scala
@@ -66,7 +66,7 @@ class MEM extends Module {
     wen     :=  mem_axi_w.AXI_AWVALID
     waddr   :=  mem_axi_w.AXI_AWADDR
     wid     :=  mem_axi_w.AXI_AWID
-    wmask   :=  mem_axi_w.AXI_WSTRB
+    wmask   :=  mem_axi_w.AXI_WSTRB.asType(Seq(Bool()))
     wdata   :=  mem_axi_w.AXI_WDATA
 
     when (wen) { 

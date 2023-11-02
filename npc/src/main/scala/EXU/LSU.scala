4b847ee2b4ea8d355752b84c0610ccbfc052f2d5
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-02 15:30:49 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-87-generic #97~20.04.1-Ubuntu SMP Thu Oct 5 08:25:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:30:49 up 2 days, 23:12,  1 user,  load average: 1.97, 1.74, 1.79

--- npc/src/main/scala/EXU/LSU.scala
+++ npc/src/main/scala/EXU/LSU.scala
@@ -61,13 +61,13 @@ class LSU extends Module {
 
     // to ex
     ls_ex.rd_wdata := MuxCase(0.U, Seq(
-        INST_LB   ->  SEXT(rd_wdata(7, 0)),
-        INST_LH   ->  SEXT(rd_wdata(15, 0)),
-        INST_LW   ->  SEXT(rd_wdata(31, 0)),
-        INST_LD   ->  SEXT(rd_wdata),
-        INST_LBU  ->  ZEXT(rd_wdata(7, 0)),
-        INST_LHU  ->  ZEXT(rd_wdata(15, 0)),
-        INST_LWU  ->  ZEXT(rd_wdata(31, 0))
+        al_ls === INST_LB   ->  SEXT(rd_wdata(7, 0)),
+        al_ls === INST_LH   ->  SEXT(rd_wdata(15, 0)),
+        al_ls === INST_LW   ->  SEXT(rd_wdata(31, 0)),
+        al_ls === INST_LD   ->  SEXT(rd_wdata),
+        al_ls === INST_LBU  ->  ZEXT(rd_wdata(7, 0)),
+        al_ls === INST_LHU  ->  ZEXT(rd_wdata(15, 0)),
+        al_ls === INST_LWU  ->  ZEXT(rd_wdata(31, 0))
     ))// load
     ls_ex.rd_waddr := al_ls.rd_waddr   
     ls_ex.rd_wen   := al_ls.inst_isload

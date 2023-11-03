598929d7c03f9b46a8a9412ed7e9b4910e47ab14
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-04 02:38:06 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:38:06 up 11:49,  1 user,  load average: 1.05, 0.95, 0.90

--- npc/src/main/scala/CTRL.scala
+++ npc/src/main/scala/CTRL.scala
@@ -57,7 +57,8 @@ class Ctrl extends Module {
     // val mem_ctrl      = IO(new MEM_CTRL_Input()     )
     val redirect_ctrl = IO(new Bundle { val rs_id_ex_hit = Input(Bool())})
     val mcif_ctrl     = IO(new Bundle {     
-        val axi_busy_start = Input(Bool())
+        val inst_fetch_busy = Input(Bool())
+        val load_store_busy = Input(Bool())
         val axi_busy_end   = Input(Bool())
     })
 
@@ -69,24 +70,27 @@ class Ctrl extends Module {
 
     val jump            = ex_ctrl.typej_jump_en   // || ex_ctrl.intr_jump_en
     
-    val axi_busy        = RegInit(0.U(1.W))      // Reg
-    val axi_busy_start  = mcif_ctrl.axi_busy_start  // wire 一个寄存器加一根线保证全覆盖
-    axi_busy := Mux(mcif_ctrl.axi_busy_start, 1.U, 
-                    Mux(mcif_ctrl.axi_busy_end, 0.U, axi_busy))
+    val inst_fetch_busy = mcif_ctrl.inst_fetch_busy
+    val load_store_busy = mcif_ctrl.load_store_busy
+    // val axi_busy        = RegInit(0.U(1.W))      // Reg
+    // val axi_busy_start  = mcif_ctrl.axi_busy_start  // wire 一个寄存器加一根线保证全覆盖
+    // axi_busy := Mux(mcif_ctrl.axi_busy_start, 1.U, 
+    //                 Mux(mcif_ctrl.axi_busy_end, 0.U, axi_busy))
     
     val load_data_hit   = redirect_ctrl.rs_id_ex_hit && ex_ctrl.inst_isload
-    val NOEVENT         = ~(jump | axi_busy | axi_busy_start | load_data_hit)
+    val NOEVENT         = ~(jump | inst_fetch_busy | load_store_busy | load_data_hit)
 
     ctrl_pc.jump_addr := ex_ctrl.typej_jump_addr //| io.clint_ctrl.intr_jump_addr
     ctrl_pc.jump_en   := jump
 
     // 给事件进行优先编码
-    val event_code = PriorityEncoder(Cat(load_data_hit, axi_busy | axi_busy_start, jump, NOEVENT)) // 从低到高输出第一个有1的位数 0->NOEVENT
+    val event_code = PriorityEncoder(Cat(load_data_hit, inst_fetch_busy, load_store_busy, jump, NOEVENT)) // 从低到高输出第一个有1的位数 0->NOEVENT
 
     //  List(pc_stall_en, if_id_stall_en, id_ex_stall_en, ex_wb_stall_en)
     val stall_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B), Array(
-        BitPat(2.U) -> List(true.B, true.B, false.B, true.B),     // axi_busy       
-        BitPat(3.U) -> List(true.B, true.B, false.B, false.B)       // load_data_hit      
+        BitPat(2.U) -> List(true.B, true.B, false.B, true.B),     // load_store_busy     
+        BitPat(3.U) -> List(true.B, true.B, false.B, false.B),  // inst_fetch_busy       
+        BitPat(4.U) -> List(true.B, true.B, false.B, false.B)     // load_data_hit      
     ))
 
     ctrl_pc.pc_stall_en         := stall_list(0)
@@ -98,8 +102,9 @@ class Ctrl extends Module {
     val flush_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B), Array(
         BitPat(0.U) -> List(false.B, false.B, false.B, false.B),   // Noevent
         BitPat(1.U) -> List(false.B, true.B,  true.B,  false.B),    // jump
-        BitPat(2.U) -> List(false.B, false.B, true.B,  false.B),    // axi_busy          
-        BitPat(3.U) -> List(false.B, false.B, true.B,  false.B)     // load_data_hit 
+        BitPat(2.U) -> List(false.B, false.B, true.B,  false.B),    // load_store_busy
+
+        BitPat(4.U) -> List(false.B, false.B, true.B,  false.B)     // load_data_hit 
     ))
 
     ctrl_pc.pc_flush_en         := flush_list(0)

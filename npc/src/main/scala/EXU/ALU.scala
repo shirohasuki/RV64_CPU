c1202af6018cb9e162adc28052ea768605c21633
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-03 19:17:56 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:17:56 up  4:29,  1 user,  load average: 2.66, 2.00, 1.61

--- npc/src/main/scala/EXU/ALU.scala
+++ npc/src/main/scala/EXU/ALU.scala
@@ -194,7 +194,9 @@ class ALU extends Module {
     al_ls.mem_ren       :=  exce_list(3)
     al_ls.mem_raddr     :=  exce_list(4)
     al_ls.mem_wen       :=  exce_list(5)
-    for(i <- 0 until 7) {al_ls.mem_wmask(i) := exce_list(6)(i+1, i)}  // UInt -> wmask
+    for(i <- 0 until 7) {
+        al_ls.mem_wmask(i) := (exce_list(6))(i+1, i)
+    }  // UInt -> wmask
         
     al_ls.mem_wdata     :=  exce_list(7)
     al_ls.mem_waddr     :=  exce_list(8)

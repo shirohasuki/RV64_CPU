28c0e1c00689df7476f15ae32e0784f9d4dbc6db
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-03 22:18:01 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:18:01 up  7:29,  1 user,  load average: 0.54, 0.30, 0.22

--- npc/src/main/scala/MCIF.scala
+++ npc/src/main/scala/MCIF.scala
@@ -161,10 +161,10 @@ class MCIF_W extends Module {
     M_WID := 1.U
 
     when(mcif_axi_r.AXI_RVALID) {
-        resp0.valid := mcif_axi_r.AXI_RVALID
-        resp1.valid := mcif_axi_r.AXI_RVALID
-        resp0.bits  := mcif_axi_r.AXI_RDATA 
-        resp1.bits  := mcif_axi_r.AXI_RDATA 
+        resp0.valid := Mux(mcif_axi_r.AXI_RID === 0.U, mcif_axi_r.AXI_RVALID, 0.U)
+        resp1.valid := Mux(mcif_axi_r.AXI_RID === 1.U, mcif_axi_r.AXI_RVALID, 0.U)
+        resp0.bits  := Mux(mcif_axi_r.AXI_RID === 0.U, mcif_axi_r.AXI_RDATA , 0.U)
+        resp1.bits  := Mux(mcif_axi_r.AXI_RID === 1.U, mcif_axi_r.AXI_RDATA , 0.U)
     }   // S->M
 
     

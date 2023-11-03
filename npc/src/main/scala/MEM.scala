165ffa7af23c8f411560910513394d12f922ee43
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-03 23:15:10 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:15:10 up  8:26,  1 user,  load average: 1.56, 1.33, 1.41

--- npc/src/main/scala/MEM.scala
+++ npc/src/main/scala/MEM.scala
@@ -87,9 +87,9 @@ class MEM extends Module {
 
     when (wen) { 
         when (wmask(7) === false.B) {mem.write(waddr >> 3, mem_axi_w.AXI_WDATA)
-        }.eleswhen (wmask(3) === false.B) {mem.write(waddr >> 3, mem_axi_w.AXI_WDATA(31, 0))
-        }.eleswhen (wmask(1) === false.B) {mem.write(waddr >> 3, mem_axi_w.AXI_WDATA(15, 0))
-        }.eleswhen (wmask(0) === false.B) {mem.write(waddr >> 3, mem_axi_w.AXI_WDATA(7, 0))}
+        }.elsewhen (wmask(3) === false.B) {mem.write(waddr >> 3, mem_axi_w.AXI_WDATA(31, 0))
+        }.elsewhen (wmask(1) === false.B) {mem.write(waddr >> 3, mem_axi_w.AXI_WDATA(15, 0))
+        }.elsewhen (wmask(0) === false.B) {mem.write(waddr >> 3, mem_axi_w.AXI_WDATA(7, 0))}
         // mem.write(waddr >> 3, wdata_vec, wmask) 
     }
 

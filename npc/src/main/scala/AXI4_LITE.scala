caf7d9e181049990733c587b033461e32fa62ba5
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-03 23:26:28 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:26:28 up  8:37,  1 user,  load average: 0.73, 1.06, 1.33

--- npc/src/main/scala/AXI4_LITE.scala
+++ npc/src/main/scala/AXI4_LITE.scala
@@ -116,7 +116,7 @@ class AXI4_LITE extends Module {
     val waddr   = RegInit(0.U(64.W))
     val wid     = RegInit(0.U(2.W))
     val wdata   = RegInit(0.U(64.W))
-    val wstrb   = Wire(Vec(8, Bool()))
+    val wstrb   = Reg(Vec(8, Bool()))
     val wstrb_default = VecInit(Seq.fill(8)(false.B))
     waddr   := Mux(mcif_axi_w.AXI_AWVALID, mcif_axi_w.AXI_AWADDR, 
                     Mux(aw_complete, 0.U, waddr)) 

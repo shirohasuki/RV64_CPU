a5ac9903862aa8dc79a9db4eeb9a5b90ba53faef
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-13 18:57:37 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:57:37 up 21 min,  1 user,  load average: 0.77, 0.98, 1.08

--- npc/src/main/scala/CTRL.scala
+++ npc/src/main/scala/CTRL.scala
@@ -101,7 +101,7 @@ class Ctrl extends Module {
 
         //  List(pc_flush_en, if_id_flush_en, id_ex_flush_en, ex_wb_flush_en)
     val flush_list  = ListLookup(event_code, List(false.B, false.B, false.B, false.B), Array(
-        BitPat("b10".U) -> List(false.B, false.B, true.B,  true.B),    // load_store_busy
+        BitPat("b10".U) -> List(false.B, false.B, false.B,  true.B),    // load_store_busy
         BitPat("b00".U) -> List(false.B, false.B, false.B, false.B),    // Noevent
         BitPat("b01".U) -> List(false.B, true.B,  true.B,  false.B),    // jump
         BitPat("b11".U) -> List(false.B, false.B, true.B,  false.B)     // load_data_hit 

8f00f6f6ad0c5b0a9e5d3e96b7c5727544be9805
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-13 21:29:50 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:29:50 up  2:54,  1 user,  load average: 0.84, 0.67, 0.76

--- npc/src/main/scala/IFU.scala
+++ npc/src/main/scala/IFU.scala
@@ -38,5 +38,5 @@ class IFU extends Module {
     if_mcif.mem_ren     := 1.U & (pc =/= 0.U) & ~(ctrl_pc.jump_en)
     if_mcif.mem_raddr   := Mux(ctrl_pc.jump_en, 0.U, pc) 
     if_ifid.inst        := Mux(if_mcif.mem_raddr(2) === 1.U, if_mcif.mem_rdata(31, 0), if_mcif.mem_rdata(63, 32))
-    if_ifid.pc          := Mux(ctrl_pc.jump_en, 0.U, pc_next) 
+    if_ifid.pc          := pc_next
 }

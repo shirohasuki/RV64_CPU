28567474aeae7c1670b313936cbc2d25ef625db0
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-15 19:18:39 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:18:39 up  6:08,  1 user,  load average: 0.72, 0.79, 0.78

--- npc/src/csrc/src/main.cpp
+++ npc/src/csrc/src/main.cpp
@@ -59,6 +59,9 @@ void npc_exec_once() {
 
 void sim_exit() {
     // step_and_dump_wave();
+    top->eval();
+    // contextp->timeInc(1);
+    // tfp->dump(contextp->time());
     printf("here\n");
     tfp->close();
     printf("The wave data has been saved to the dump.vcd\n");

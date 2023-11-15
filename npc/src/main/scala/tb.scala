2204f1e31dfdb4ecb8d982902ab630abeec556c0
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-15 15:46:37 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:46:37 up  2:36,  1 user,  load average: 2.01, 1.69, 1.49

--- npc/src/main/scala/tb.scala
+++ npc/src/main/scala/tb.scala
@@ -21,7 +21,7 @@ import MEM._
 class tb extends Module {
     val CORE        = Module(new CORE())
     // val AXI4_LITE   = Module(new AXI4_LITE())
-    // val MEM         = Module(new MEM())
+    val MEM         = Module(new MEM())
     
     // CORE.core_axi_r <> AXI4_LITE.mcif_axi_r
     // CORE.core_axi_w <> AXI4_LITE.mcif_axi_w

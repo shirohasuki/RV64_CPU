4296ded6efdc029e39b1519ca6132e3ac00a9633
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-15 22:34:13 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:34:13 up  9:24,  1 user,  load average: 1.81, 1.26, 1.05

--- npc/src/csrc/src/utils/utils.c
+++ npc/src/csrc/src/utils/utils.c
@@ -60,14 +60,14 @@ void dump_csr() {
 
 
 void npc_exit(int status) {
-    if (status == 0) puts(ASNI_FMT("[Sim Result]: HIT GOOD TRAP", ASNI_FG_GREEN));
+    if (status == 0) printf(ASNI_FMT("[Sim Result]: HIT GOOD TRAP\n", ASNI_FG_GREEN));
     else {
         IFDEF(CONFIG_NPC_ITRACE, itrace_output());
         IFDEF(CONFIG_NPC_MTRACE, print_mtrace());
         IFDEF(CONFIG_NPC_GPRTRACE, dump_gpr());
-	    printf(ASNI_FMT("NPC meets error at step %d", ASNI_FG_BLUE), nemu_step);
+	    printf(ASNI_FMT("NPC meets error at step %d\n", ASNI_FG_BLUE), nemu_step);
         // IFDEF(CONFIG_NPC_GPRTRACE, dump_csr());
-        printf(ASNI_FMT("[Sim Result]: HIT BAD TRAP", ASNI_FG_RED));
+        printf(ASNI_FMT("[Sim Result]: HIT BAD TRAP\n", ASNI_FG_RED));
     }
     sim_exit();
     exit(status);

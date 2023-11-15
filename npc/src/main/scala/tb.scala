3e376715df4ef4963658852e356a42aca24ae20a
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-15 15:56:43 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:56:43 up  2:46,  1 user,  load average: 0.19, 0.45, 0.91

--- npc/src/main/scala/tb.scala
+++ npc/src/main/scala/tb.scala
@@ -21,7 +21,7 @@ import MEM._
 class tb extends Module {
     val CORE        = Module(new CORE())
     // val AXI4_LITE   = Module(new AXI4_LITE())
-    val MEM         = Module(new MEM())
+    // val MEM         = Module(new MEM())
     
     // CORE.core_axi_r <> AXI4_LITE.mcif_axi_r
     // CORE.core_axi_w <> AXI4_LITE.mcif_axi_w
@@ -29,7 +29,7 @@ class tb extends Module {
     // AXI4_LITE.mem_axi_r <> MEM.mem_axi_r
     // AXI4_LITE.mem_axi_w <> MEM.mem_axi_w
 
-    loadMemoryFromFileInline(MEM.mem, "./image.hex", MemoryLoadFileType.Hex); 
+    // loadMemoryFromFileInline(MEM.mem, "./image.hex", MemoryLoadFileType.Hex); 
     // loadMemoryFromFile只会在chiseltest的时候生成readmemb
 }
 

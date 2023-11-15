c5d638ed24ade85594d0453afea04d05f22679b8
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-16 02:09:21 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:09:21 up 12:59,  1 user,  load average: 1.81, 1.57, 1.55

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -27,7 +27,7 @@ run: image
 #因为位宽为64所以使用sed两行合并为一行
 	@echo '======================= copy .bin -> .hex finish ============================='
 # $(MAKE) -C $(NPC_HOME)
-	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(ARGS)" IMG=$(IMAGE).bin
 # make 定义了很多默认变量，${MAKE} 就是预设的 make 这个命令的名称（或者路径）
 #-C dir　在读取 makefile 之前改变到指定的目录dir。
 # 生成完image后，去NPC_HOME下执行该make

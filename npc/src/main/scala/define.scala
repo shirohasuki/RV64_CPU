49ba0203c6470b9c9de7cad7a1121105be266181
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-16 21:35:46 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:35:46 up  1:40,  1 user,  load average: 0.58, 1.19, 1.43

--- npc/src/main/scala/define.scala
+++ npc/src/main/scala/define.scala
@@ -19,9 +19,9 @@ object function {
         data_o
     }
 
-    // 这两个都是用在64位的，要拓展得改
-    def SEXT (short: UInt): UInt = Cat(Fill(32, short(short.getWidth-1)), short(short.getWidth-1, 0))
-    def ZEXT (short: UInt): UInt = Cat(Fill(32,   0.U), short(short.getWidth-1, 0)) // zero extension 瞎取的名字，向前补0
+    // 这两个都是用在拓展到64位的，要拓展得改
+    def SEXT (short: UInt): UInt = Cat(Fill(64 - short.getWidth, short(short.getWidth-1)), short(short.getWidth-1, 0))
+    def ZEXT (short: UInt): UInt = Cat(Fill(32 - short.getWidth,   0.U), short(short.getWidth-1, 0)) // zero extension 瞎取的名字，向前补0
 } 
 
 // object DPI_C {

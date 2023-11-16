76a80cbddfcd4a3f495af771098a24165bccdb05
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-16 22:20:30 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:20:30 up  2:24,  1 user,  load average: 1.70, 0.97, 0.89

--- npc/src/main/scala/define.scala
+++ npc/src/main/scala/define.scala
@@ -22,10 +22,10 @@ object function {
     // 这两个都是用在拓展到64位的，要拓展得改
     def SEXT (short: UInt): UInt = {
         val SEXT_data = Wire(UInt(64.W))
-        SEXT_data := Cat(Fill(64-short.getWidth, short(short.getWidth-1)), short(short.getWidth-1, 0))
+        SEXT_data := Cat(Fill(64-signedBitLength(short.getWidth), short(short.getWidth-1)), short(short.getWidth-1, 0))
         SEXT_data
     }
-    def ZEXT (short: UInt): UInt = Cat(Fill(32-short.getWidth,   0.U), short(short.getWidth-1, 0)) // zero extension 瞎取的名字，向前补0
+    def ZEXT (short: UInt): UInt = Cat(Fill(64-short.getWidth,   0.U), short(short.getWidth-1, 0)) // zero extension 瞎取的名字，向前补0
 } 
 
 // object DPI_C {

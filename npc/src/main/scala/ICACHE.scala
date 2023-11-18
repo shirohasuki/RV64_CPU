61b252eb70ad9b4cc17ab2d6ce23a10390b17d97
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-18 21:03:44 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:03:44 up 1 day,  4:18,  1 user,  load average: 0.92, 1.32, 1.08

--- npc/src/main/scala/ICACHE.scala
+++ npc/src/main/scala/ICACHE.scala
@@ -108,8 +108,9 @@ class ICACHE extends Module {
     val DPIC_pmem_read  = Module(new pmem_read())
     when (ren && miss) {
         DPIC_pmem_read.io.raddr         := raddr
-        dataMem(idx_reg)(offset_reg)    := DPIC_pmem_read.io.rdata   
         vMem    :=  vMem.bitSet(idx_reg, true.B) 
+        tagMem  :=  tagMem.write(idx_reg, DPIC_pmem_read.io.rdata(63, 12))
+        dataMem(idx_reg)(offset_reg)    := DPIC_pmem_read.io.rdata   
         reload_complete                 := 1.U
     }.otherwise {
         // dataMem(idx_reg)(offset_reg)    := dataMem(idx_reg)(offset_reg) 

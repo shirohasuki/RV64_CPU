40331758a9e948fb566c073cf82b47e84d31cc2f
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-19 21:53:10 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:53:10 up 2 days,  5:07,  1 user,  load average: 1.51, 2.05, 2.07

--- npc/src/csrc/src/utils/mem.c
+++ npc/src/csrc/src/utils/mem.c
@@ -105,7 +105,7 @@ extern "C" void pmem_read_cacheline(ll raddr, svBitVecVal rdata[8]) {
         for (int j = 0; j < 4; j++) {
             ret = (ret << 8) | (*pt--);
         } 
-        *pt += 3;
+        *pt += 7;
         for (int k = 0; k < 4; k++) {
             ret = (ret << 8) | (*pt--);
         } 

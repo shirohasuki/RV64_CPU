abd4ab0fe06f399756f72adc7beda31c867ce734
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-19 21:46:09 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:46:09 up 2 days,  5:00,  1 user,  load average: 2.83, 2.28, 2.07

--- npc/src/csrc/src/utils/mem.c
+++ npc/src/csrc/src/utils/mem.c
@@ -97,13 +97,13 @@ extern "C" void pmem_read(ll raddr, ll *rdata) {
 
 extern "C" void pmem_read_cacheline(ll raddr, svBitVecVal rdata[8]) {
     if (raddr < MEM_BASE) { return ; } 
-    uint8_t *pt = cpu2mem(raddr)+7; // 指向64个字节的末尾
+    uint8_t *pt = cpu2mem(raddr) + 7; // 指向64个字节的末尾
     ll ret = 0;
     // for (int i = 0; i < 8; i++) {
         // *pt += 7;
         int i = 0;
         for (int j = 0; j < 4; j++) {
-            ret = (ret << 4) | (*pt--);
+            ret = (ret << 8) | (*pt--);
         } 
         // for (int j = 0; j < 8; j++) {
         //     ret = (ret << 8) | (*pt--);

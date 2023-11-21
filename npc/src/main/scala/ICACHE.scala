b72c6c4cffd7da9ac7b292c511895a095310a550
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-22 01:14:03 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:14:03 up  8:09,  1 user,  load average: 2.58, 2.06, 1.52

--- npc/src/main/scala/ICACHE.scala
+++ npc/src/main/scala/ICACHE.scala
@@ -134,17 +134,19 @@ class ICACHE extends Module {
 
     when (ren) {
         DPIC_pmem_read_cacheline.io.raddr       := Cat(raddr(63, 6), Fill(6, 0.U))
-        val writeData = Vec(Seq(
-            DPIC_pmem_read_cacheline.io.rdata(0), 
-            DPIC_pmem_read_cacheline.io.rdata(1), 
-            DPIC_pmem_read_cacheline.io.rdata(2), 
-            DPIC_pmem_read_cacheline.io.rdata(3),
-            DPIC_pmem_read_cacheline.io.rdata(4), 
-            DPIC_pmem_read_cacheline.io.rdata(5), 
-            DPIC_pmem_read_cacheline.io.rdata(6), 
-            DPIC_pmem_read_cacheline.io.rdata(7)
-        ))
-        dataMem.write(writeAddress, writeData)
+        // val writeData = Vec(Seq(
+        //     DPIC_pmem_read_cacheline.io.rdata(0), 
+        //     DPIC_pmem_read_cacheline.io.rdata(1), 
+        //     DPIC_pmem_read_cacheline.io.rdata(2), 
+        //     DPIC_pmem_read_cacheline.io.rdata(3),
+        //     DPIC_pmem_read_cacheline.io.rdata(4), 
+        //     DPIC_pmem_read_cacheline.io.rdata(5), 
+        //     DPIC_pmem_read_cacheline.io.rdata(6), 
+        //     DPIC_pmem_read_cacheline.io.rdata(7)
+        // ))
+        // dataMem.write(writeAddress, writeData)
+        for (i <- 0 until 8) { dataMem.write(writeAddress, Seq(DPIC_pmem_read_cacheline.io.rdata(i)))}
+
         // for (i <- 0 until 8) { dataMem(idx)(i)  := DPIC_pmem_read_cacheline.io.rdata(i)}
         vMem                                    := vMem.bitSet(idx, true.B) 
         tagMem(idx)                             := tag

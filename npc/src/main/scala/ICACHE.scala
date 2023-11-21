dca6fd20ecb0f55fd91fb793be08f82a1b1c20a0
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-21 22:49:20 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:49:20 up  5:44,  1 user,  load average: 1.82, 1.41, 1.11

--- npc/src/main/scala/ICACHE.scala
+++ npc/src/main/scala/ICACHE.scala
@@ -130,12 +130,9 @@ class ICACHE extends Module {
 
     when (ren) {
         DPIC_pmem_read_cacheline.io.raddr       := Cat(raddr(63, 6), Fill(6, 0.U))
-
-        val writeAddress = 5.U
-        val writeData = VecInit(Seq.fill(8)(DPIC_pmem_read_cacheline.io.rdata(i)))
-        
-            // dataMem.write(writeAddress, writeData)
         for (i <- 0 until 8) {
+                    val writeAddress = 5.U
+        val writeData = VecInit(Seq.fill(8)(DPIC_pmem_read_cacheline.io.rdata(i)))
             dataMem.write(writeAddress, writeData)
         }
         // for (i <- 0 until 8) { dataMem(idx)(i)  := DPIC_pmem_read_cacheline.io.rdata(i)}

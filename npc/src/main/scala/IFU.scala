6e1e9c4ac306efd05a8cad1cee9a2ce5a3197469
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-21 18:48:40 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:48:40 up  1:44,  1 user,  load average: 0.91, 0.83, 0.85

--- npc/src/main/scala/IFU.scala
+++ npc/src/main/scala/IFU.scala
@@ -32,11 +32,11 @@ class IFU extends Module {
         val pc    = Input(UInt(64.W))
     })
     
-    val pc_reg = RegInit(0.U(64.W)) 
-    pc_reg := pc_if.pc
+    // val pc_reg = RegInit(0.U(64.W)) 
+    // pc_reg := pc_if.pc
 
     if_icache.req.valid       := pc_if.pc =/= 0.U      // ren
     if_icache.req.bits.raddr  := pc_if.pc 
     if_ifid.inst              := Mux(if_icache.req.bits.raddr(2) === 1.U, if_icache.resp.bits.rdata(31, 0), if_icache.resp.bits.rdata(63, 32))
-    if_ifid.pc                := Mux(if_icache.resp.valid, pc_reg, 0.U)
+    if_ifid.pc                := Mux(if_icache.resp.valid, pc_if.pc, 0.U)
 }

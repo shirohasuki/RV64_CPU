fdb634c016688e85606228f76e5101901104ffec
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-21 22:03:10 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:03:10 up  4:58,  1 user,  load average: 1.66, 1.36, 1.32

--- npc/src/main/scala/IFU.scala
+++ npc/src/main/scala/IFU.scala
@@ -11,6 +11,7 @@ package IFU
 import chisel3._
 import chisel3.util._
 import chisel3.stage._
+import define.MACRO._
 
 class CacheReq extends Bundle { val raddr = UInt(64.W) }
 class CacheResp extends Bundle { val rdata = UInt(64.W) }
@@ -38,6 +39,6 @@ class IFU extends Module {
     if_icache.req.valid       := pc_if.pc =/= 0.U      // ren
     if_icache.req.bits.raddr  := pc_if.pc 
     if_ifid.inst              := Mux(if_icache.resp.valid,  
-                                    Mux(if_icache.req.bits.raddr(2) === 0.U, if_icache.resp.bits.rdata(31, 0), if_icache.resp.bits.rdata(63, 32)), 0.U)
+                                    Mux(if_icache.req.bits.raddr(2) === 0.U, if_icache.resp.bits.rdata(31, 0), if_icache.resp.bits.rdata(63, 32)), INST_NOP)
     if_ifid.pc                := Mux(if_icache.resp.valid, pc_if.pc, 0.U)
 }

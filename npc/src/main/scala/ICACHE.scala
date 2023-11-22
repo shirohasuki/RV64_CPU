dd8d24231544dea38aa282dd89c7a7fcc2d15d62
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-22 23:01:32 +0800

    > sim RTL ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:01:32 up 1 day,  5:57,  1 user,  load average: 2.91, 1.95, 1.39

--- npc/src/main/scala/ICACHE.scala
+++ npc/src/main/scala/ICACHE.scala
@@ -140,11 +140,9 @@ class ICACHE extends Module {
 
     when (ren && miss) {
         DPIC_pmem_read_cacheline.io.raddr       := Cat(raddr(63, 6), Fill(6, 0.U))
-
         // val writeAddress = idx
         // val writeData    = VecInit.tabulate(8)(i => DPIC_pmem_read_cacheline.io.rdata(i))
         // dataMem.write(writeAddress, writeData)
-
         for (i <- 0 until 8) { dataMem(idx)(i)  := DPIC_pmem_read_cacheline.io.rdata(i)}
 
         tagMem(idx)                             := tag

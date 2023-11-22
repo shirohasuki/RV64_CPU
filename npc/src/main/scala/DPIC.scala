d494b3dfba92e13533315a23a911e79a00aab937
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-23 01:38:35 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:38:35 up 1 day,  8:34,  1 user,  load average: 2.07, 1.54, 1.47

--- npc/src/main/scala/DPIC.scala
+++ npc/src/main/scala/DPIC.scala
@@ -249,7 +249,7 @@ class ctrace extends BlackBox with HasBlackBoxInline {
     })
     setInline("ctrace.v",
     """
-    |import "DPI-C" function void ctrace_record(input byte idx, input longint tag, input logic[7:0] cacheline);
+    |import "DPI-C" function void ctrace_record(input byte idx, input longint tag, input logic[63:0] cacheline[8]);
     |module ctrace (
     |   input  [5:0]  idx,
     |   input  [51:0] tag,

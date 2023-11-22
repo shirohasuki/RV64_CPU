f39c475669f5a4fb074321741ab8c2e756494637
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-22 22:59:22 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:59:22 up 1 day,  5:54,  1 user,  load average: 1.71, 1.55, 1.20

--- npc/src/main/scala/ICACHE.scala
+++ npc/src/main/scala/ICACHE.scala
@@ -162,9 +162,9 @@ class ICACHE extends Module {
 
     // 7. output
     val icache_miss     = next_state === sMiss | state === sMiss
-    val icache_latency  = WireInit(false.B)
-    when (state === sHit) {
-        icache_latency := 1.U & (icache_latency === 0.U)
+    val icache_latency  = RegInit(false.B)
+    when (next_state === sHit && ~icache_latency) {
+        icache_latency := 1.U
     }.otherwise {
         icache_latency := 0.U
     }

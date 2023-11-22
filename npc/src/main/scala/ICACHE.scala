2e962e2ca35395703bf7d47e8d8535eb0e827e31
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-22 18:23:21 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:23:21 up 1 day,  1:18,  1 user,  load average: 2.86, 2.41, 2.00

--- npc/src/main/scala/ICACHE.scala
+++ npc/src/main/scala/ICACHE.scala
@@ -117,14 +117,14 @@ class ICACHE extends Module {
     val rdata_test5 = WireInit(0.U(64.W))
     val rdata_test6 = WireInit(0.U(64.W))
     val rdata_test7 = WireInit(0.U(64.W))
-    // rdata_test0 := dataMem(4)(0)
-    // rdata_test1 := dataMem(4)(1)
-    // rdata_test2 := dataMem(4)(2)
-    // rdata_test3 := dataMem(4)(3)
-    // rdata_test4 := dataMem(4)(4)
-    // rdata_test5 := dataMem(4)(5)
-    // rdata_test6 := dataMem(4)(6)
-    // rdata_test7 := dataMem(4)(7)
+    rdata_test0 := dataMem(idx)(0)
+    rdata_test1 := dataMem(idx)(1)
+    rdata_test2 := dataMem(idx)(2)
+    rdata_test3 := dataMem(idx)(3)
+    rdata_test4 := dataMem(idx)(4)
+    rdata_test5 := dataMem(idx)(5)
+    rdata_test6 := dataMem(idx)(6)
+    rdata_test7 := dataMem(idx)(7)
 
     
 

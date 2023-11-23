2cda5997521e8858fc801980e8c3c8e6861fd28a
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-23 14:14:08 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-88-generic #98~20.04.1-Ubuntu SMP Mon Oct 9 16:43:45 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:14:08 up 1 day, 21:09,  1 user,  load average: 1.03, 1.08, 1.15

--- npc/src/csrc/src/utils/trace.c
+++ npc/src/csrc/src/utils/trace.c
@@ -96,6 +96,7 @@ extern "C" void ctrace_record(char idx, ll tag, const svOpenArrayHandle cachelin
 
 void print_ctrace() {
     puts("========== CTRACE Result ==========");
+    puts("ICache BUFF");
     printf("idx\ttag\t||=======off0======||=======off1======||=======off2======||=======off3======||=======off4======||=======off5======||=======off6======||=======off7======||\n");
     for (int idx = 0; idx < SIZE_CTRACEBUF; idx++) {
         if (icache_buf[idx][0] == 0) break; // valid == 0

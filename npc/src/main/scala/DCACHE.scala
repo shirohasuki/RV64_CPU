032f49df6f168d80a60b0ade4669fc2613a7dd3c
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-25 20:06:42 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-89-generic #99~20.04.1-Ubuntu SMP Thu Nov 2 15:16:47 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:06:42 up 1 day,  5:34,  1 user,  load average: 0.81, 1.34, 1.61

--- npc/src/main/scala/DCACHE.scala
+++ npc/src/main/scala/DCACHE.scala
@@ -171,19 +171,19 @@ class DCACHE extends Module {
     // update 伪LRU 最终ageMem的值对应最近使用次数最少的那一行
     ageMem(set_idx).bitSet(6, Mux(way_idx > 3.U, true.B, false.B))
 
-    ageMem(set_idx).bitSet(5, Mux(ageMem(6) === 1.U, ageMem(5),  // 维持原值
+    ageMem(set_idx).bitSet(5, Mux(ageMem(6) === 1.U, ageMem(5) & 1.U,  // 维持原值
                     Mux(way_idx > 1.U, true.B, false.B))) 
-    ageMem(set_idx).bitSet(4, Mux(ageMem(6) === 0.U, ageMem(4),  // 维持原值
+    ageMem(set_idx).bitSet(4, Mux(ageMem(6) === 0.U, ageMem(4) & 1.U,  // 维持原值
                     Mux(way_idx > 5.U, true.B, false.B)))
 
-    ageMem(set_idx).bitSet(3, Mux(ageMem(5) === 1.U, ageMem(3),  // 维持原值
+    ageMem(set_idx).bitSet(3, Mux(ageMem(5) === 1.U, ageMem(3) & 1.U,  // 维持原值
                     Mux(way_idx === 1.U, true.B, false.B)))
-    ageMem(set_idx).bitSet(2, Mux(ageMem(5) === 0.U, ageMem(2),  // 维持原值
+    ageMem(set_idx).bitSet(2, Mux(ageMem(5) === 0.U, ageMem(2) & 1.U,  // 维持原值
                     Mux(way_idx === 3.U, true.B, false.B))) 
 
-    ageMem(set_idx).bitSet(1, Mux(ageMem(4) === 1.U, ageMem(1),  // 维持原值
+    ageMem(set_idx).bitSet(1, Mux(ageMem(4) === 1.U, ageMem(1) & 1.U,  // 维持原值
                     Mux(way_idx === 5.U, true.B, false.B))) 
-    ageMem(set_idx).bitSet(0, Mux(ageMem(4) === 0.U, ageMem(0),  // 维持原值
+    ageMem(set_idx).bitSet(0, Mux(ageMem(4) === 0.U, ageMem(0) & 1.U,  // 维持原值
                     Mux(way_idx === 7.U, true.B, false.B))) 
 
 

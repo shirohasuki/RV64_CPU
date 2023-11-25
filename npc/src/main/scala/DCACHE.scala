0f30587c5c67bd0d5acd4956d8c49553d2ff71cf
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-25 19:29:59 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-89-generic #99~20.04.1-Ubuntu SMP Thu Nov 2 15:16:47 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:29:59 up 1 day,  4:58,  1 user,  load average: 2.40, 1.70, 1.55

--- npc/src/main/scala/DCACHE.scala
+++ npc/src/main/scala/DCACHE.scala
@@ -55,7 +55,7 @@ class DCACHE extends Module {
 
     val tag     = raddr(63, 9)
     // val set_idx : Int = raddr(8, 5).litValue.toInt
-    val set_idx : Int = raddr(8, 5)
+    val set_idx = raddr(8, 5)
     val way_idx = WireInit(0.U(3.W))
     val offset  = raddr(4, 3)
     val rdata   = WireInit(0.U(64.W))

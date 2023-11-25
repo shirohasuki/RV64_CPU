051f385d77bb91da5f4763b2e8b645d8ee3e4e44
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-25 23:14:56 +0800

    > generate verilog ysyx_22040058 王晖 Linux shiroha 5.15.0-89-generic #99~20.04.1-Ubuntu SMP Thu Nov 2 15:16:47 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:14:56 up 1 day,  8:43,  1 user,  load average: 1.22, 1.27, 1.20

--- npc/src/csrc/src/utils/trace.c
+++ npc/src/csrc/src/utils/trace.c
@@ -127,7 +127,7 @@ void print_ctrace() {
         }
     }
     puts("\n========== DCache ");
-    printf("set  idx  tag    ||======off0======||======off1======||======off2======||======off3======||\n");
+    printf("set==idx==||=tag==||======off0======||======off1======||======off2======||======off3======||\n");
     for (int set_idx = 0; set_idx < DCACHE_SETNUM; set_idx++) {
         if (dcache_buf[set_idx][0][0] == 0) continue; // valid == 0
         printf("%2d   ", set_idx);
@@ -135,7 +135,7 @@ void print_ctrace() {
         for (int way_idx = 0; way_idx < DCACHE_WAYNUM; way_idx++) {
             if (dcache_buf[set_idx][way_idx][0] == 0) continue; // valid == 0
 
-            printf("%2d  %6llx  ", way_idx, dcache_buf[set_idx][way_idx][3]); // idx和tag
+            printf("%2d  ||%6llx", way_idx, dcache_buf[set_idx][way_idx][3]); // idx和tag
 
             for (int offset = 0; offset < 4; offset++) {
                 printf("||%016llx", dcache_buf[set_idx][way_idx][4 + offset]);
